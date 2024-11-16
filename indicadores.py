c5a929f152eb508cae581cdca0ce6046504904c0
miraki <derquicia@mariogoldsteinsa.com.ar>
2024-11-16 19:18:01 -0300

    Update indicadores.py

--- indicadores.py
+++ indicadores.py
@@ -87,6 +87,21 @@ with col[0]:
     st.markdown('Mostos')
     st.metric(label='HL', value= 201909 , delta=102.98)
     st.metric(label='FOB', value= 46389836 , delta=85.97)
+    b = (
+        Bar()
+        .add_xaxis(["Microsoft", "Amazon", "IBM", "Oracle", "Google", "Alibaba"])
+        .add_yaxis("2017-2018 Revenue in (billion $)", random.sample(range(100), 10))
+        .set_global_opts(
+            title_opts=opts.TitleOpts(
+                title="Top cloud providers 2018", subtitle="2017-2018 Revenue"
+            ),
+            toolbox_opts=opts.ToolboxOpts(),
+        )
+    )
+    st_pyecharts(
+        b, key="echarts"
+    )  # Add key argument to not remount component at every Streamlit run
+    
     
 with col[1]:
     st.markdown('Mercado Interno')
