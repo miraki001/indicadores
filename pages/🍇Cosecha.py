e8af70d89e694d53aefbf625ef88e85bb99e8fe0
miraki <derquicia@mariogoldsteinsa.com.ar>
2024-11-18 09:37:22 -0300

    Update 🍇Cosecha.py

--- pages/🍇Cosecha.py
+++ pages/🍇Cosecha.py
@@ -1,6 +1,6 @@
 
 import streamlit as st
 
-with st.popover("Open popover"):
-    st.markdown("Hello World 👋")
+with st.popover("Abrir Filtros"):
+    st.markdown("Filtros 👋")
     name = st.text_input("What's your name?")
