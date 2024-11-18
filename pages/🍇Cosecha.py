31f1214bbce994bf6044f3ade17f2bdd9afc8ff9
miraki <derquicia@mariogoldsteinsa.com.ar>
2024-11-18 09:38:10 -0300

    Update 🍇Cosecha.py

--- pages/🍇Cosecha.py
+++ pages/🍇Cosecha.py
@@ -4,3 +4,15 @@ import streamlit as st
 with st.popover("Abrir Filtros"):
     st.markdown("Filtros 👋")
     name = st.text_input("What's your name?")
+
+tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
+
+with tab1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+with tab2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+with tab3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
