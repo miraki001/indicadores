1cd7220e258e87235540a07bdddf5b826c9cea27
miraki <derquicia@mariogoldsteinsa.com.ar>
2024-11-18 10:46:49 -0300

    Update 🍇Cosecha.py

--- pages/🍇Cosecha.py
+++ pages/🍇Cosecha.py
@@ -9,10 +9,16 @@ year_list = list(df.anio.unique())[::-1]
 dv = conn.query('select distinct variedad from superficievariedad_m ;', ttl="0")
 var_list = list(dv.variedad.unique())[::-1]
 
+dv = conn.query('select distinct provincia from superficievariedad_m ;', ttl="0")
+prov_list = list(dv.variedad.unique())[::-1]
+color_list =  ("Tinto", "Blanco","Rosado","Sin Dato")
+
 with st.popover("Abrir Filtros"):
     st.markdown("Filtros 👋")
     anio = st.selectbox( "Año :", year_list )
     var = st.selectbox( "Variedad :", var_list )
+    prov = st.selectbox( "Provincia :", prov_list )
+    color = st.selectbox( "Color :", color_list )
     st.button("Ok", type="primary")
 
 
