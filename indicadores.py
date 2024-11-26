72cafe39bfef27f3c2bb31b47199871f6f706a47
miraki <derquicia@mariogoldsteinsa.com.ar>
2024-11-26 12:48:51 -0300

    Update indicadores.py

--- indicadores.py
+++ indicadores.py
@@ -277,6 +277,19 @@ with tab2:
 
 
         option = {
+        "color": [
+            '#dd6b66',
+            '#759aa0',
+            '#e69d87',
+            '#8dc1a9',
+            '#ea7e53',
+            '#eedd78',
+            '#73a373',
+            '#73b9bc',
+            '#7289ab',
+            '#91ca8c',
+            '#f49f42'
+        ],            
         "tooltip": {
             "trigger": "item"
         },    
