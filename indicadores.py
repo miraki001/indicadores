5df474f2a28112d6d050bf9cd0c2f80b7cf98eed
miraki <derquicia@mariogoldsteinsa.com.ar>
2024-11-26 13:03:07 -0300

    Update indicadores.py

--- indicadores.py
+++ indicadores.py
@@ -332,7 +332,7 @@ with tab2:
         "label": {
             "alignTo": 'edge',
 #            "formatter": '{name|{b}}\n{time|{c} }',
-            "formatter": '{name }',
+            "formatter": '{name|{b}} ,
             "minMargin": 5,
             "edgeDistance": 10,
             "lineHeight": 15,
