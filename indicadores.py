cfb4d6a7056411a569f9cc4f5dbfe6399204ef08
miraki <derquicia@mariogoldsteinsa.com.ar>
2024-11-26 11:47:30 -0300

    Update indicadores.py

--- indicadores.py
+++ indicadores.py
@@ -290,7 +290,7 @@ with tab2:
         {
             "name": "año 2023",
             "type": "pie",
-            "radius": ["30%", "70%"],
+            "radius": ["50%", "70%"],
             "center": ["50%", "70%"],
             "startAngle": 180,
             "endAngle": 360,
