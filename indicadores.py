c265893c1617d852d6e99def7ee8735bf732d0d0
miraki <derquicia@mariogoldsteinsa.com.ar>
2024-11-26 13:22:45 -0300

    Update indicadores.py

--- indicadores.py
+++ indicadores.py
@@ -341,7 +341,7 @@ with tab2:
             "trigger": "item"
         },    
         "legend": {
-            "top": "15%",
+            "top": "1%",
             "left": "center" 
             },
         "label": {
