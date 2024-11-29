b5a401509f1861dcd44f94dfe27505b049d1f76b
miraki <derquicia@mariogoldsteinsa.com.ar>
2024-11-29 14:30:31 -0300

    Update 🍷Mercado Interno.py

--- pages/🍷Mercado Interno.py
+++ pages/🍷Mercado Interno.py
@@ -163,7 +163,8 @@ with col2:
 st.write("You selected:", prov)
 qu = 'select cnt,provincia from inf_desp_prov where provincia =  + %s    or  + %s  = "Todas" ;'
 st.write(qu)
-dfpv1 = conn.query(qu,prov, ttl="0"),
+#dfpv1 = conn.query(qu,prov, ttl="0"),
+dfpv1 = conn.execute(qu,prov),
 dfpv2 = dfpv1[0]
 st.write(dfpv2)
 
