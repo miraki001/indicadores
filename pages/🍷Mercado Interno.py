4ea1fefe01f1f6275b0f6d637dd1853efd691417
miraki <derquicia@mariogoldsteinsa.com.ar>
2024-11-29 13:44:50 -0300

    Update 🍷Mercado Interno.py

--- pages/🍷Mercado Interno.py
+++ pages/🍷Mercado Interno.py
@@ -138,7 +138,9 @@ st.subheader('Evolución de los despachos por Provincias')
 conn = st.connection("postgresql", type="sql")
 dfp = conn.query('select provincia from dimprovincia;', ttl="0"),
 dfpv = dfp[0]
-dfpv = dfpv[0] + "Todas",
+
+dfpv = pd.concat([dfpv, "Todas"], ignore_index=True)
+
 st.write(dfpv)
 
 option = st.selectbox(
