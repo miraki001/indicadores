aaec090634292d2c2012d947f2b10e6003b0d7e2
miraki <derquicia@mariogoldsteinsa.com.ar>
2024-11-30 11:39:09 -0300

    Update 🍷Mercado Interno.py

--- pages/🍷Mercado Interno.py
+++ pages/🍷Mercado Interno.py
@@ -172,7 +172,7 @@ st.write(dfpv1)
 
 dfpv2 = dfpv1[0].transpose()
 st.write(dfpv2)
-dfpv3 = dfpv2,filter(anio=[2021,2022,2023])
+dfpv3 = dfpv2,filter(anio=["2021","2022","2023"])
 st.write(dfpv3)
 
 #dfpv2['anio'] = dfpv2['anio'].astype(str)
