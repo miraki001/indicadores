068254f33fc683a29844c0c1c23a0c423f387bd4
miraki <derquicia@mariogoldsteinsa.com.ar>
2024-11-30 12:48:27 -0300

    Update 🍷Mercado Interno.py

--- pages/🍷Mercado Interno.py
+++ pages/🍷Mercado Interno.py
@@ -175,7 +175,7 @@ df = dfpv1.pivot_table(index='provincia', columns='anio', values='cnt')
 st.write(df)
 df = df.reset_index() 
 st.write(df)
-st.write(df[1])
+st.write(df['provincia'])
 
 dfpv2 = dfpv1.transpose()
 st.write(dfpv2)
