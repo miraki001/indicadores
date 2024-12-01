da4bfb4330b112ae37f01a8d87cfa0cdcc6805a1
miraki <derquicia@mariogoldsteinsa.com.ar>
2024-12-01 09:43:46 -0300

    Update 🍷Mercado Interno.py

--- pages/🍷Mercado Interno.py
+++ pages/🍷Mercado Interno.py
@@ -211,34 +211,51 @@ option = {
         "type": "category",
         "data": df['anio'].to_list(),
     },
-    "yAxis": [
-    {
-      "type": 'value',
-    },
-    ],
+    "yAxis": [{"type": "value"}],
     "series": [
-     {
-      "name": 'Email',
-      "type": 'line',
-      "stack": 'Total',
-      "areaStyle": {},
-      "emphasis": {
-        "focus": 'series'
-      },
-      "data": [120, 132, 101, 134, 90, 230, 210]
-    },
-    {
-      "name": 'Union Ads',
-      "type": 'line',
-      "stack": 'Total',
-      "areaStyle": {},
-      "emphasis": {
-        "focus": 'series'
-      },
-      "data": [220, 182, 191, 234, 290, 330, 310]
-    },
-   
-  ]
+            {
+                "name": "邮件营销",
+                "type": "line",
+                "stack": "总量",
+                "areaStyle": {},
+                "emphasis": {"focus": "series"},
+                "data": [120, 132, 101, 134, 90, 230, 210],
+            },
+            {
+                "name": "联盟广告",
+                "type": "line",
+                "stack": "总量",
+                "areaStyle": {},
+                "emphasis": {"focus": "series"},
+                "data": [220, 182, 191, 234, 290, 330, 310],
+            },
+            {
+                "name": "视频广告",
+                "type": "line",
+                "stack": "总量",
+                "areaStyle": {},
+                "emphasis": {"focus": "series"},
+                "data": [150, 232, 201, 154, 190, 330, 410],
+            },
+            {
+                "name": "直接访问",
+                "type": "line",
+                "stack": "总量",
+                "areaStyle": {},
+                "emphasis": {"focus": "series"},
+                "data": [320, 332, 301, 334, 390, 330, 320],
+            },
+            {
+                "name": "搜索引擎",
+                "type": "line",
+                "stack": "总量",
+                "label": {"show": True, "position": "top"},
+                "areaStyle": {},
+                "emphasis": {"focus": "series"},
+                "data": [820, 932, 901, 934, 1290, 1330, 1320],
+            },
+        ],
+    }
   """
     "series": [{"data": df['MENDOZA'].to_list() ,"type": "line", "name": 'Mendoza'},
                {"data": df['SAN JUAN'].to_list(), "type": "line", "name": 'San Juan'},
