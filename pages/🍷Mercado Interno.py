1cce847a72291627dcf76869ece6bf23b50f7832
miraki <derquicia@mariogoldsteinsa.com.ar>
2024-12-02 13:39:23 -0300

    Update 🍷Mercado Interno.py

--- pages/🍷Mercado Interno.py
+++ pages/🍷Mercado Interno.py
@@ -201,11 +201,9 @@ option = {
   "yAxis": [{ "gridIndex": 0 }, { "gridIndex": 1 }],
   "grid": [{ "bottom": '55%' }, { "top": '55%' }],
   "series": [
-    // These series are in the first grid.
     { "type": 'bar', "seriesLayoutBy": 'row' },
     { "type": 'bar', "seriesLayoutBy": 'row' },
     { "type": 'bar', "seriesLayoutBy": 'row' },
-    // These series are in the second grid.
     { "type": 'bar', "xAxisIndex": 1, "yAxisIndex": 1 },
     { "type": 'bar', "xAxisIndex": 1, "yAxisIndex": 1 },
     { "type": 'bar', "xAxisIndex": 1, "yAxisIndex": 1 },
