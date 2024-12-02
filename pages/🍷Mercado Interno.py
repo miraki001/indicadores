3791d682e8f934656ea4d04c9874c84647e54199
miraki <derquicia@mariogoldsteinsa.com.ar>
2024-12-02 14:06:29 -0300

    Update 🍷Mercado Interno.py

--- pages/🍷Mercado Interno.py
+++ pages/🍷Mercado Interno.py
@@ -203,8 +203,6 @@ option = {
   },
   "xAxis":  { type: 'category' },
   yAxis: {},
-  // Declare several bar series, each will be mapped
-  // to a column of dataset.source by default.
   "series": [{ "type": 'bar' }, { "type": 'bar' }, { "type": 'bar' }]
 };
 st_echarts(
