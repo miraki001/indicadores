e186d0d209e036039bd21387d981f890bdc3288b
miraki <derquicia@mariogoldsteinsa.com.ar>
2024-12-03 16:23:57 -0300

    Update 🍷Mercado Interno.py

--- pages/🍷Mercado Interno.py
+++ pages/🍷Mercado Interno.py
@@ -517,7 +517,7 @@ option = {
                 "stack": "cnt",
                 "areaStyle": {},
                 "emphasis": {"focus": "series"},
-                "data": prod['Vino sin Mension'].to_list(),
+                "data": prod['Vino sin Mencion'].to_list(),
             },
             {
                 "name": "Espumantes",
