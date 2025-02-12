6414a7d585d7eb9fd85f9f9296aec41787baa881
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-02-12 16:16:37 -0300

    Update 🚚Mosto.py

--- pages/🚚Mosto.py
+++ pages/🚚Mosto.py
@@ -1,1 +1,13 @@
+import streamlit as st
+import streamlit.components.v1 as components
+from pivottablejs import pivot_ui
+import pandas as pd
 
+iris = pd.read_csv(
+    "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv"
+)
+
+t = pivot_ui(iris)
+
+with open(t.src) as t:
+    components.html(t.read(), width=900, height=1000, scrolling=True)
