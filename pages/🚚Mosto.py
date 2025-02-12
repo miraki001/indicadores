1ac93ce3f125384a0e4769a7ef37f07e0e5f45e9
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-02-12 16:59:08 -0300

    Update 🚚Mosto.py

--- pages/🚚Mosto.py
+++ pages/🚚Mosto.py
@@ -1,24 +1,13 @@
 import streamlit as st
 import streamlit.components.v1 as components
-from pivottablejs import pivot_ui
 import pandas as pd
 import json
 
-iris = pd.read_csv(
-    "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv"
-)
 
 qu = 'select año anio,sum(cnt) cnt,provincia,subgrupoenvase from inf_desp_prov group by provincia,año,subgrupoenvase ;'  
 dfpv1 = conn.query(qu, ttl="0"),
-#if prov != "Todas": 
-#  qu = 'select cnt,provincia from inf_desp_prov where provincia =  :prov;'
-#  dfpv1 = conn.query(qu, ttl="0", params={"prov": prov},),
 dfpv1 = dfpv1[0]
 dfpv1 = dfpv1[dfpv1['anio'] > 2010]
-#dfpv1 = dfpv1[dfpv1['provincia'].isin(prov)]
-#st.write(dfpv1)
 
-t = pivot_ui(dfpv1)
-
-with open(t.src) as t:
-    components.html(t.read(), width=900, height=1000, scrolling=True)
+pivot = pd.pivot_table(dfpv1, values=“cnt”, index=[‘anio’,‘provincia’],columns=[‘subgrupoenvase’], aggfunc={‘cnt’: [min,max,np.mean,np.std]} )
+st.dataframe(pivot)
