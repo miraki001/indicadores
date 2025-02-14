347ad9ec41da104e76dbaec255208b096cb9347a
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-02-14 12:53:51 -0300

    Update 🚜Superficie.py

--- pages/🚜Superficie.py
+++ pages/🚜Superficie.py
@@ -12,6 +12,7 @@ conn = st.connection("postgresql", type="sql")
 qu = 'select * from despachos_m ;'  
 dfpv1 = conn.query(qu, ttl="0"),
 dfpv1 = dfpv1[0]
+dfpv1 = dfpv1[dfpv1['anio'] > 2014]
 data = dfpv1
 st.write(data)
 
