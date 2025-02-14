0606aaba5ada23bafa88bde59c998fc56ad11cb7
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-02-14 12:57:33 -0300

    Update 🚜Superficie.py

--- pages/🚜Superficie.py
+++ pages/🚜Superficie.py
@@ -14,40 +14,9 @@ dfpv1 = conn.query(qu, ttl="0"),
 dfpv1 = dfpv1[0]
 dfpv1 = dfpv1[dfpv1['anio'] > 2014]
 data = dfpv1
-st.write(data)
-
-gb = GridOptionsBuilder()
-
-gb.configure_default_column(
-    resizable=True,
-    filterable=True,
-    sortable=True,
-    editable=False,
-)
+#st.write(data)
 
 
-gb.configure_grid_options(
-    autoGroupColumnDef=dict(
-        minWidth=600, 
-        enableRangeSelection=True,
-        enableRangeHandle=True,
-        pinned="left", 
-        cellRendererParams=dict(suppressCount=True)
-    )
-)
-
-gb.configure_grid_options(
-    tooltipShowDelay=0,
-    pivotMode=True,
-)
-
-gb.configure_selection(
-    'multiple',
-    use_checkbox=False,
-)
-
-go = gb.build()
-
-grid_response  = AgGrid(data, gridOptions=go,height=500)
+AgGrid(data,height=500)
 
 
