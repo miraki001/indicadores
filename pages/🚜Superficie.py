71479557e04c609c2a3b69b3540efcb91b3a15b6
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-02-14 12:40:04 -0300

    Update 🚜Superficie.py

--- pages/🚜Superficie.py
+++ pages/🚜Superficie.py
@@ -1,1 +1,13 @@
+import streamlit as st
+import streamlit.components.v1 as components
+import pandas as pd
+import json
+from st_aggrid import AgGrid, GridOptionsBuilder,DataReturnMode,GridUpdateMode
+import altair as alt
+import numpy as np
 
+
+
+conn = st.connection("postgresql", type="sql")
+qu = 'select * from despachos_m ;'  
+dfpv1 = conn.query(qu, ttl="0"),
