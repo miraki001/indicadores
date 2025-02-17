fc71d8949989237a0824fc8e386aa8e8c0bc0149
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-02-17 11:30:49 -0300

    Update 🏪Consumo Mercado Interno.py

--- pages/🏪Consumo Mercado Interno.py
+++ pages/🏪Consumo Mercado Interno.py
@@ -60,7 +60,7 @@ st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 st.markdown(""" <style> #MainMenu {visibility: hidden;} footer {visibility: hidden;} </style> """, unsafe_allow_html=True)
 
 conn = st.connection("postgresql", type="sql")
-df = conn.query('SELECT periodo,"CERVEZAS","VINOS_COMUNES","VINOS_FINOS","APERITIVOS_ALC","APERITIVOS_RTD","ESPUMANTES","FRIZANTES","SIDRAS_Y_SABORES","VINOS_FORTIFICADOS" FROM scentia_res;', ttl="0")
+df = conn.query('SELECT periodo,canal,"CERVEZAS","VINOS_COMUNES","VINOS_FINOS","APERITIVOS_ALC","APERITIVOS_RTD","ESPUMANTES","FRIZANTES","SIDRAS_Y_SABORES","VINOS_FORTIFICADOS" FROM scentia_res;', ttl="0")
 
 st.subheader('Ventas en el Canal Mayorista, Según datos de Scentia')
 
