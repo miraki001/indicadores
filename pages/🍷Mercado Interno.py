d437d80d5ed99fc90bcf82a02df7d99f5f2871ab
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-02-17 19:38:06 -0300

    Update 🍷Mercado Interno.py

--- pages/🍷Mercado Interno.py
+++ pages/🍷Mercado Interno.py
@@ -601,6 +601,6 @@ with tab2:
     st.write(pivot_table_basic)
     st.write(pivot_table_basic.columns.tolist())
     st.write(pivot_table_basic[2022])
-    pivot_table_basic['var 23'] = (pivot_table_basic[2023]/pivot_table_basic[2022])
+    pivot_table_basic['202-23'] = (1-(pivot_table_basic[2023]/pivot_table_basic[2022]))*100
     st.write(pivot_table_basic)
   
