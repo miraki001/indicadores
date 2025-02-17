a7701e470aea937558aff35f377b96a05e0e7c32
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-02-17 19:49:06 -0300

    Update 🍷Mercado Interno.py

--- pages/🍷Mercado Interno.py
+++ pages/🍷Mercado Interno.py
@@ -602,6 +602,6 @@ with tab2:
     st.write(pivot_table_basic.columns.tolist())
     st.write(pivot_table_basic[2022])
     pivot_table_basic['2022dif'] = (1-(pivot_table_basic[2022]/pivot_table_basic[2023]))*100
-    pivot_table_basic = pivot_table_basic.reindex(sorted(pivot_table_basic.columns), axis=1)
+    pivot_table_basic = pivot_table_basic.sort_index(axis=1)
     st.write(pivot_table_basic)
   
