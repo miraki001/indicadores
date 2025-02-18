329bbb51553015bcef9c3e86a0c6996a3efcaec7
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-02-18 10:45:40 -0300

    Update 🍷Mercado Interno.py

--- pages/🍷Mercado Interno.py
+++ pages/🍷Mercado Interno.py
@@ -627,6 +627,7 @@ with tab2:
         '2023/2022': st.column_config.Column('2023/2022'),
         '2024/2023': st.column_config.Column('2024/2023')
       },
+      width = 600,                 
       hide_index=False)
     #st.write(pivot_table_basic)
   
