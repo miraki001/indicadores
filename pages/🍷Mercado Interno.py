9a7807363032542c73c9786bb2a701db0ce8a498
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-02-18 13:34:27 -0300

    Update 🍷Mercado Interno.py

--- pages/🍷Mercado Interno.py
+++ pages/🍷Mercado Interno.py
@@ -593,7 +593,7 @@ with tab2:
 
     with col2:
         selected_categories = st.multiselect('Productos:', df2['producto'].unique())
-        st.write(selected_categories)
+        st.write(selected_categories[0])
         # Filter the dataframe based on selected categories
         df2 = df2[df2['producto'].isin(selected_categories)]
         
