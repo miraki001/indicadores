7e1564e9c526ab962956a274ecabebe63a83fbcb
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-02-18 13:46:20 -0300

    Update 🍷Mercado Interno.py

--- pages/🍷Mercado Interno.py
+++ pages/🍷Mercado Interno.py
@@ -569,7 +569,7 @@ def filter_by_producto(df, airline):
 
 
 with tab2:
-    @st.cache(allow_output_mutation=True)
+    @st.cache
     df3 = conn.query('select cantidadlitros lts,anio,mes,provincia,producto,subgrupoenvase,variedad1 from despachos_m  where anio > 2021  ;', ttl="0"),
 
     df2 = df3[0]
