1319db2892aef086053dd047a3a45cf7aa49e98e
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-02-19 14:31:30 -0300

    Update desp_todo.py

--- util/desp_todo.py
+++ util/desp_todo.py
@@ -1,1 +1,11 @@
 
+conn = st.connection("postgresql", type="sql")
+qu = 'select * from despachos_m ;'  
+dfpv1 = conn.query(qu, ttl="0"),
+dfpv1 = dfpv1[0]
+dfpv1 = dfpv1[dfpv1['anio'] > 2014]
+data = dfpv1
+#st.write(data)
+
+
+AgGrid(data,height=500)
