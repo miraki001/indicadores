14cecf0f588bcfa5184de4596c9bc387ff1c7065
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-02-19 17:10:44 -0300

    Update 🚜Superficie.py

--- pages/🚜Superficie.py
+++ pages/🚜Superficie.py
@@ -23,37 +23,42 @@ def query():
   return df3
 df3 = query()
 df2 = df3[0]
-#st.write(df2)
-total = []
-tot1 = []
-total.append(0)
-tot1.append(0)
-for index in range(len(df2)):
-  if index > 0:
-    total.append((  (df2['supeficie'].loc[index] / df2['supeficie'].loc[index -1]) -1 ) *100 )
-    tot1.append((  (df2['cant_viñedos'].loc[index] / df2['cant_viñedos'].loc[index -1]) -1 ) *100 )
-#st.write(total)
-df2 = df2.rename(columns={'supeficie': "Superficie", 'cant_viñedos': "Viñedos Cnt.",'año': "Año"})
-df2['Superficie Var %'] = total
-df2['Viñedos Var. %'] = tot1
 
-df2 = df2.sort_index(axis = 1)
+tab1, tab2, tab3,tab4 = st.tabs(["Evolución", "Por Provincia y Color", "Por Provincia y Color","Por Variedad"])
 
-styled_df = df2.style.applymap(bgcolor_positive_or_negative, subset=['Superficie Var %','Viñedos Var. %']).format(
+with tab1:
+
+
+    total = []
+    tot1 = []
+    total.append(0)
+    tot1.append(0)
+    for index in range(len(df2)):
+      if index > 0:
+        total.append((  (df2['supeficie'].loc[index] / df2['supeficie'].loc[index -1]) -1 ) *100 )
+        tot1.append((  (df2['cant_viñedos'].loc[index] / df2['cant_viñedos'].loc[index -1]) -1 ) *100 )
+    #st.write(total)
+    df2 = df2.rename(columns={'supeficie': "Superficie", 'cant_viñedos': "Viñedos Cnt.",'año': "Año"})
+    df2['Superficie Var %'] = total
+    df2['Viñedos Var. %'] = tot1
+
+    df2 = df2.sort_index(axis = 1)
+
+    styled_df = df2.style.applymap(bgcolor_positive_or_negative, subset=['Superficie Var %','Viñedos Var. %']).format(
         {"Superficie": lambda x : '{:,.0f}'.format(x), 
         "Viñedos Cnt.": lambda x : '{:,.0f}'.format(x),
         "Superficie Var %": lambda x : '{:,.2f} %'.format(x),
         "Viñedos Var. %": lambda x : '{:,.2f} %'.format(x),
                                         }
         ,
-    thousands='.',
-    decimal=',',
-)
+        thousands='.',
+        decimal=',',
+    )
 
 
-#st.write(df2)
+    #st.write(df2)
 
-st.dataframe(styled_df,
+    st.dataframe(styled_df,
       column_config={
         'Año': st.column_config.Column('Año'),
         'Superficie': st.column_config.Column('Superficie'),
@@ -61,12 +66,12 @@ st.dataframe(styled_df,
         'Superficie Var %': st.column_config.Column('Superficie Var %'),
         'Viñedos Var. %': st.column_config.Column('Viñedos Var. %'),
         
-    },
-    width = 600,   
-    height = 800,
-    hide_index=True)
+        },
+        width = 600,   
+        height = 800,
+        hide_index=True)
 
-option = {
+    option = {
         "dataZoom": [
         {
           "show": 'true',
@@ -97,8 +102,8 @@ option = {
                ,{"data": df2['Viñedos Cnt.'].to_list(), "type": "line","name":'Cnt. de Viñedos'}
                  
                   ]
-}
-st_echarts(
+    }
+    st_echarts(
         options=option, height="400px" ,
-)
+    )
 
