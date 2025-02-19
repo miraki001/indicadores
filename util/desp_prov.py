44a1581888e4f425c9dbc72336cf4feeddfdeae9
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-02-19 08:22:04 -0300

    Update desp_prov.py

--- util/desp_prov.py
+++ util/desp_prov.py
@@ -10,3 +10,16 @@ from pyecharts import options as opts
 from pyecharts.charts import Line
 from streamlit_echarts import Map
 from st_keyup import st_keyup
+
+def despachos_prov(df):
+      pivot_table_basic = df.pivot_table(
+      index='provicnia', 
+      columns='anio',  
+      values=['lts'],
+      aggfunc='sum'
+    )
+    
+    dfg = pivot_table_basic
+    dfg.columns = dfg.columns.droplevel(0)
+    dfg = dfg.rename(columns={2022: "2022", 2023: "2023", 2024: "2024",'mes': " mes"})
+    st.write(dfg) 
