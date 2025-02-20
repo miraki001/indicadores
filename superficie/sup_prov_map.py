dd42c372be5eba19f9a64aa59cfeee1debc2f487
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-02-20 14:47:20 -0300

    Update sup_prov_map.py

--- superficie/sup_prov_map.py
+++ superficie/sup_prov_map.py
@@ -12,7 +12,7 @@ from pyecharts.charts import Bar
 from pyecharts import options as opts
 from pyecharts.charts import Line
 
-def pov_map(df)
+def pov_map(df):
 
   f = df.to_json(orient="records")
 
