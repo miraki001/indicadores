af86c8c62145d204a814dd7a5edd099eb8be0d9f
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-02-20 08:51:00 -0300

    Update sup_prov_color.py

--- superficie/sup_prov_color.py
+++ superficie/sup_prov_color.py
@@ -1,1 +1,15 @@
+import streamlit as st
+import streamlit.components.v1 as components
+import pandas as pd
+import json
+from st_aggrid import AgGrid, GridOptionsBuilder,DataReturnMode,GridUpdateMode
+import altair as alt
+import numpy as np
+from streamlit_echarts import st_echarts
+from streamlit_echarts import JsCode
+from streamlit_echarts import st_pyecharts
+from pyecharts.charts import Bar
+from pyecharts import options as opts
+from pyecharts.charts import Line
 
+def sup_prov_color(df):
