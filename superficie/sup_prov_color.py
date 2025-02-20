f73257f6235dfe7d51bc1abea5167b5c5340b910
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-02-20 12:24:15 -0300

    Update sup_prov_color.py

--- superficie/sup_prov_color.py
+++ superficie/sup_prov_color.py
@@ -13,3 +13,22 @@ from pyecharts import options as opts
 from pyecharts.charts import Line
 
 def sup_prov_color(df):
+  pivot_table_basic = df.pivot_table(
+      index='provincia', 
+      columns='color',  
+      values=['sup'],
+      aggfunc='sum'
+  )
+  st.write(pivot_table_basic)  
+  dfg = pivot_table_basic
+  dfg.columns = dfg.columns.droplevel(0)
+  #st.write(dfg) 
+    
+  #pivot_table_basic.columns = pivot_table_basic.columns.droplevel(0)
+  pivot_table_basic = pivot_table_basic.reset_index().rename_axis(None, axis=1)
+  pivot_table_basic.loc['Total']= pivot_table_basic.sum(numeric_only=True,axis=0)
+  pivot_table_basic['2023/2022'] = (1-(pivot_table_basic[2022]/pivot_table_basic[2023]))*100
+  pivot_table_basic['2024/2023'] = (1-(pivot_table_basic[2023]/pivot_table_basic[2024]))*100
+  pivot_table_basic = pivot_table_basic.rename(columns={2022: "2022", 2023: "2023", 2024: "2024",'mes': " mes"})
+
+  pivot_table_basic = pivot_table_basic.sort_index(axis = 1)
