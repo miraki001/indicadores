5a7d1257764ef7b8aa833ccec37089f12a60c7f1
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-02-20 14:44:37 -0300

    Update sup_prov_map.py

--- superficie/sup_prov_map.py
+++ superficie/sup_prov_map.py
@@ -1,4 +1,20 @@
- f = data[0].to_json(orient="records")
+import streamlit as st
+import streamlit.components.v1 as components
+import pandas as pd
+import json
+from st_aggrid import AgGrid, GridOptionsBuilder,DataReturnMode,GridUpdateMode
+import altair as alt
+import numpy as np
+from streamlit_echarts import st_echarts
+from streamlit_echarts import JsCode
+from streamlit_echarts import st_pyecharts
+from pyecharts.charts import Bar
+from pyecharts import options as opts
+from pyecharts.charts import Line
+
+def pov_map(df)
+
+  f = df.to_json(orient="records")
 
   #st.write(f)  
 
@@ -20,7 +36,7 @@
   
   options = {
         "title": {
-            "text": "Despachos por Provincias",
+            "text": "Superficie por Provincias",
 #            "subtext": "Data from www.census.gov",
 #            "sublink": "http://www.census.gov/popest/data/datasets.html",
             "left": "right",
