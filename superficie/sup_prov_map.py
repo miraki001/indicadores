2eabd011359496fe44d7f1cd9f26c848c39a007a
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-02-21 13:58:53 -0300

    Update sup_prov_map.py

--- superficie/sup_prov_map.py
+++ superficie/sup_prov_map.py
@@ -13,6 +13,24 @@ from pyecharts import options as opts
 from streamlit_echarts import Map
 from pyecharts.charts import Line
 import geopandas as gpd
+import plotly.express as px
+
+
+def make_choropleth(input_df, input_id, input_column, input_color_theme):
+    choropleth = px.choropleth(input_df, locations=input_id, color=input_column, locationmode="USA-states",
+                               color_continuous_scale=input_color_theme,
+                               range_color=(0, max(df_selected_year.population)),
+                               scope="usa",
+                               labels={'population':'Population'}
+                              )
+    choropleth.update_layout(
+        template='plotly_dark',
+        plot_bgcolor='rgba(0, 0, 0, 0)',
+        paper_bgcolor='rgba(0, 0, 0, 0)',
+        margin=dict(l=0, r=0, t=0, b=0),
+        height=350
+    )
+    return choropleth
 
 def prov_map(df):
 
