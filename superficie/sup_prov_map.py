0ddb8a6ef7d5ea869eea9883047ebc733df5125a
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-02-21 15:33:22 -0300

    Update sup_prov_map.py

--- superficie/sup_prov_map.py
+++ superficie/sup_prov_map.py
@@ -139,7 +139,7 @@ def prov_map(df):
   st_echarts(options, map=map)
   #st.map(pivot_table_basic)
 
-  fig = px.choropleth(locations=["AR-A","AR-B","AR-C"],locationmode="ISO-3", color=[1,2,3])
+  fig = px.choropleth(df1, locations='iso_loc',locationmode="ISO-3", color=[1,2,3])
  
   fig.show()
 
