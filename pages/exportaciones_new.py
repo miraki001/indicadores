018a3369c4bb90e50c3c558ad8840952d8d6c615
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-03-25 16:15:46 -0300

    Update exportaciones_new.py

--- pages/exportaciones_new.py
+++ pages/exportaciones_new.py
@@ -1,1 +1,51 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import json
+from streamlit_echarts import st_echarts
+from streamlit_echarts import JsCode
+from streamlit_echarts import st_pyecharts
+from pyecharts.charts import Bar
+from pyecharts import options as opts
+from pyecharts.charts import Line
 
+
+hide_streamlit_style = """
+                <style>
+                div[data-testid="stToolbar"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                div[data-testid="stDecoration"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                div[data-testid="stStatusWidget"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                #MainMenu {
+                visibility: hidden;
+                height: 0%;
+                }
+                header {
+                visibility: hidden;
+                height: 0%;
+                }
+                footer {
+                visibility: hidden;
+                height: 0%;
+                }
+                </style>
+                """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+
+
+
+conn = st.connection("postgresql", type="sql")
+df = conn.query('select anio,litros,fob from inf_expo_anio ;', ttl="0")
+#st.write(df)
