80ba7927728a7cce4e801468919c42b5e201a332
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-03-26 15:47:25 -0300

    Update exportaciones_new.py

--- pages/exportaciones_new.py
+++ pages/exportaciones_new.py
@@ -92,7 +92,7 @@ with st.popover("Abrir Filtros"):
     producto = st.multiselect("Producto:", producto_list, default=["Todos"])
 
     if st.button("Aplicar filtros", type="primary"):
-        st.session_state.filtros = {"anio": anio, "var": var, "envase": envase, "color": vcolor,"producto": producto}
+        st.session_state.filtros = {"anio": anio, "var": var, "envase": envase, "vcolor": vcolor,"producto": producto}
         st.rerun()  # Vuelve a ejecutar la app para aplicar los filtros
 
 # Obtener filtros aplicados
