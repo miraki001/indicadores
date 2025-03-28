635bf7051563178d74bdaaf1077e46fa12dee95b
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-03-28 13:41:57 -0300

    Update exportaciones_new.py

--- pages/exportaciones_new.py
+++ pages/exportaciones_new.py
@@ -171,7 +171,7 @@ QUERY_V2 = f"""
 dv1 = cargar_datos(QUERY_V1)
 dv2 = cargar_datos(QUERY_V2)
 
-pivot = pd.pivot_table(dv2, values=["litros","fob","ppl"], index=['mes'],columns=['anio'])
+pivot = pd.pivot(dv2, values=["litros","fob","ppl"], index=['mes'],columns=['anio'])
 st.dataframe(pivot)
 
 #dv2 = dv1
