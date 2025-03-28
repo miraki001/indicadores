b85c755a0ef2ac12ba2f8510416cc629969c82bf
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-03-28 19:23:33 -0300

    Update exportaciones_new.py

--- pages/exportaciones_new.py
+++ pages/exportaciones_new.py
@@ -346,7 +346,7 @@ else:
         "series": [
             {"data": anio1.tolist(), "type": "line", "name": "Litros", },
             {"data": anio2.tolist(), "type": "line", "name": "Fob",},
-            {"data": anio3.tolist(), "type": "line", "name": "Precio x Lts, "color":'#07ECFA', },
+            {"data": anio3.tolist(), "type": "line", "name": "Precio x Lts", "color":'#07ECFA', },
         ],
     }
 
