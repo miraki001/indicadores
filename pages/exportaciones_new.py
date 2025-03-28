3400b3362528276d1be856de82173a11b80b03d1
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-03-28 16:32:57 -0300

    Update exportaciones_new.py

--- pages/exportaciones_new.py
+++ pages/exportaciones_new.py
@@ -176,6 +176,9 @@ pivot = pd.pivot_table(dv2, index=['mes'],columns=['anio'], aggfunc='sum')
 #st.dataframe(pivot)
 st.write(pivot)
 
+pivot.columns = pivot.columns.droplevel(0)
+pivot = pivot.reset_index().rename_axis(None, axis=1)
+st.write(pivot)
 #dv2 = dv1
 #st.write(dv2)
 actual = dt.now().year -4 
