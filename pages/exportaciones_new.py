4db378e8498f9d77684cc755cf8472132a743912
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-03-28 17:32:24 -0300

    Update exportaciones_new.py

--- pages/exportaciones_new.py
+++ pages/exportaciones_new.py
@@ -181,8 +181,8 @@ litros = dv2.pivot_table(
 )
 st.write(litros)
 
-litros.columns = litros.columns.droplevel(0)
-litros = litros.reset_index().rename_axis(None, axis=2)
+litros.columns = litros.columns.droplevel(1)
+litros = litros.reset_index().rename_axis(None, axis=1)
 st.write(litros)
 #pivot = pd.pivot_table(dv2, index=['mes'],columns=['anio'], aggfunc='sum') 
 #dv3 = dv2.transpose()
