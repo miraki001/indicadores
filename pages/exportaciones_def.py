36a76e4492c6be6fe83921fea259fb24912c2b75
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-03-29 11:17:37 -0300

    Update exportaciones_def.py

--- pages/exportaciones_def.py
+++ pages/exportaciones_def.py
@@ -1,1 +1,48 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import json
+from streamlit_echarts import st_echarts
+from streamlit_echarts import JsCode
+from streamlit_echarts import st_pyecharts
+from pyecharts.charts import Bar
+from pyecharts import options as opts
+from pyecharts.charts import Line
+from datetime import datetime as dt
 
+def bgcolor_positive_or_negative(value):
+    bgcolor = "#EC654A" if value < 0 else "lightgreen"
+    return f"background-color: {bgcolor};"
+
+hide_streamlit_style = """
+                <style>
+                div[data-testid="stToolbar"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                div[data-testid="stDecoration"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                div[data-testid="stStatusWidget"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                #MainMenu {
+                visibility: hidden;
+                height: 0%;
+                }
+                header {
+                visibility: hidden;
+                height: 0%;
+                }
+                footer {
+                visibility: hidden;
+                height: 0%;
+                }
+                </style>
+                """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
