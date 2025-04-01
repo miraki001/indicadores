fee792e28ae6c2aefca4a846814b05aedf64f968
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-04-01 16:00:57 -0300

    Update exporta_pais.py

--- exportaciones/exporta_pais.py
+++ exportaciones/exporta_pais.py
@@ -145,6 +145,7 @@ def exporta_destino():
             st.write(variedad)
 
 
-    df_anual = df_filtered.groupby(['pais'], as_index='fob')[['fob', 'litros']].sum()
+    df_anual = df_filtered.groupby(['pais'], as_index=False)[['fob', 'litros']].sum()
+    df_anual.sort_values(by=¨['fob'])
     st.write("Tabla de Datos: ", df_anual)
 
