0dc0c4fbd5525fc05bf4a9dc72c5507705dc23c7
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-04-02 10:51:02 -0300

    Update exporta_pais.py

--- exportaciones/exporta_pais.py
+++ exportaciones/exporta_pais.py
@@ -146,7 +146,7 @@ def exporta_destino():
 
 
     df_anual = df_filtered.groupby(['pais'], as_index=False)[['fob', 'litros']].sum()
-
+    dv = df_anual.copy()
     total = []
     tot1 = []
     tot2 = []
@@ -173,8 +173,8 @@ def exporta_destino():
     df_sorted = df_anual.sort_values(by='Fob', ascending=False)
 
     st.dataframe(df_sorted)
-    df_filtered = df_filtered.rename(columns={'litros': "values", 'pais': "name",})
-    json_list = json.loads(json.dumps(list(df_filtered.T.to_dict().values()))) 
+    dv = dv.rename(columns={'litros': "values", 'pais': "name",})
+    json_list = json.loads(json.dumps(list(dv.T.to_dict().values()))) 
     st.subheader('Exportaciones por Variedad')
     st.write(json_list)
 
