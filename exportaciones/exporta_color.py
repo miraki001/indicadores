39ba3729a36b26e34b9e240dec7de26d80320ee2
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-04-02 20:05:53 -0300

    Update exporta_color.py

--- exportaciones/exporta_color.py
+++ exportaciones/exporta_color.py
@@ -180,7 +180,7 @@ def exporta_color():
         "legend": {"orient": "vertical", "left": "left",},
         "series": [
             {
-                "name": json_list['name'],
+                "name": json_list,
                 "type": "pie",
                 "radius": "50%",
                 "data":json_list,
