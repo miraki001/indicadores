0eb530baefcc5e27d9238b1ceb0e3f08f4787d35
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-04-02 10:40:42 -0300

    Update exporta_pais.py

--- exportaciones/exporta_pais.py
+++ exportaciones/exporta_pais.py
@@ -154,6 +154,7 @@ def exporta_destino():
     tot1.append(0)
     tot2.append(0)
     #df_anual.columns = df_anual.columns.droplevel(0)
+    st.write(df_anual)
     df_anual = df_anual.reset_index().rename_axis(None, axis=1)
     totlitros = df_anual.groupby['litros'].sum()
     totfob = df_anual.groupby['fob'].sum()
