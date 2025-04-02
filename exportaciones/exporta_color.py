ec4b7fb40a0291c9f4789db8019d52ee191f7fac
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-04-02 18:20:26 -0300

    Update exporta_color.py

--- exportaciones/exporta_color.py
+++ exportaciones/exporta_color.py
@@ -155,7 +155,7 @@ def exporta_color():
     tot2.append(0)
     #df_anual.columns = df_anual.columns.droplevel(0)
     #st.write(df_anual['litros'])
-    df_anual = df_anual.reset_index().rename_axis(None, axis=1)
+    #df_anual = df_anual.reset_index().rename_axis(None, axis=1)
     totlitros = df_anual['litros'].sum()
     totfob = df_anual['fob'].sum()
     for index in range(len(df_anual)-1):
