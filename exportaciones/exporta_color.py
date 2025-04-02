c949c52f593772537343564217b61fb905b281eb
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-04-02 18:05:52 -0300

    Update exporta_color.py

--- exportaciones/exporta_color.py
+++ exportaciones/exporta_color.py
@@ -100,7 +100,7 @@ def exporta_color():
     )
 
     QUERY_V1 = f"""
-        SELECT anio, cantlitros AS litros, valorfobsolo AS fob,variedad1,tipo_envase,pais
+        SELECT anio, cantlitros AS litros, valorfobsolo AS fob,variedad1,tipo_envase,color,pais
         FROM exportaciones2_m 
     """
 
