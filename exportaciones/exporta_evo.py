3f6f8f4b2237505068c23b437863998d6043a6b9
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-04-03 12:32:24 -0300

    Update exporta_evo.py

--- exportaciones/exporta_evo.py
+++ exportaciones/exporta_evo.py
@@ -51,6 +51,14 @@ def exporta_evolucion():
 
     #st.write(dt.now().year)
 
+    streamlit_style = """
+        <style>
+        iframe[title="streamlit_echarts.st_echarts"]{ height: 1000px;} 
+       </style>
+        """
+    st.markdown(streamlit_style, unsafe_allow_html=True) 
+
+
 
     conn = st.connection("postgresql", type="sql")
 
