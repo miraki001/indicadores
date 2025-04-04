8b41408b1ceca878ddf3abe1d41c6f1383703552
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-04-04 15:07:22 -0300

    Update mosto_evo.py

--- exportaciones/mosto_evo.py
+++ exportaciones/mosto_evo.py
@@ -294,7 +294,7 @@ def exporta_mosto_evo():
             "Fob": "{:.0f}"
             })
         #dv4.columns = dv4.columns.droplevel(0)
-        dv4.set_index("Año", inplace=True)
+        dv4.hide(axis="index", **kwargs)
         st.write(dv4)
         placeholder = st.empty()
         # Crear gráfico de líneas y barras
