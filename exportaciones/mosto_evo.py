18f0140d498fc535af9a90e567842e779f6fb53d
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-04-04 19:15:02 -0300

    Update mosto_evo.py

--- exportaciones/mosto_evo.py
+++ exportaciones/mosto_evo.py
@@ -318,7 +318,7 @@ def exporta_mosto_evo():
                     "show": 'True',
                   },              
                  "axisLabel": {
-                    "formatter": '{value/1000000} M'
+                    "formatter": '{value} M'
                       }
                 } ,
                 {"type": "value" , "name" : "eee",
