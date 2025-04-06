3f23aa8a51b76bb04abfa46f652d5bf4fb425a31
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-04-06 12:34:09 -0300

    Update exporta_pais1.py

--- exportaciones/exporta_pais1.py
+++ exportaciones/exporta_pais1.py
@@ -10,38 +10,7 @@ from datetime import datetime as dt
 def exporta_destino():
 
 
-    hide_streamlit_style = """
-                <style>
-                div[data-testid="stToolbar"] {
-                visibility: hidden;
-                height: 0%;
-                position: fixed;
-                }
-                div[data-testid="stDecoration"] {
-                visibility: hidden;
-                height: 0%;
-                position: fixed;
-                }
-                div[data-testid="stStatusWidget"] {
-                visibility: hidden;
-                height: 0%;
-                position: fixed;
-                }
-                #MainMenu {
-                visibility: hidden;
-                height: 0%;
-                }
-                header {
-                visibility: hidden;
-                height: 0%;
-                }
-                footer {
-                visibility: hidden;
-                height: 0%;
-                }
-                </style>
-                """
-    st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
 
     streamlit_style = """
         <style>
