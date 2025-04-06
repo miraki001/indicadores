ba3fdd829b1015713c83c7efb7a06b18f9f00438
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-04-06 11:44:55 -0300

    Update prueba.py

--- pages/prueba.py
+++ pages/prueba.py
@@ -1,7 +1,38 @@
 import json
 from streamlit_echarts import st_echarts
 import streamlit as st
-
+hide_streamlit_style = """
+                <style>
+                div[data-testid="stToolbar"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                div[data-testid="stDecoration"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                div[data-testid="stStatusWidget"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                #MainMenu {
+                visibility: hidden;
+                height: 0%;
+                }
+                header {
+                visibility: hidden;
+                height: 0%;
+                }
+                footer {
+                visibility: hidden;
+                height: 0%;
+                }
+                </style>
+                """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 streamlit_style = """
         <style>
