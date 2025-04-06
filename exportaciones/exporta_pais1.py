7387a769dc8770b1b381a847634ed659449fb54b
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-04-06 12:35:31 -0300

    Update exporta_pais1.py

--- exportaciones/exporta_pais1.py
+++ exportaciones/exporta_pais1.py
@@ -19,6 +19,51 @@ def exporta_destino():
         """
     st.markdown(streamlit_style, unsafe_allow_html=True)     
 
+    with open("./data/producto.json", "r") as f:
+        data = json.loads(f.read())
+
+
+
+    option = {
+        "title": {"text": "Sankey Diagram"},
+        "tooltip": {"trigger": "item", "triggerOn": "mousemove"},
+        "series": [
+            {
+                "type": "sankey",
+                "data": data["nodes"],
+                "links": data["links"],
+                "emphasis": {"focus": "adjacency"},
+                "levels": [
+                    {
+                        "depth": 0,
+                        "itemStyle": {"color": "#fbb4ae"},
+                        "lineStyle": {"color": "source", "opacity": 0.6},
+                    },
+                    {
+                        "depth": 1,
+                        "itemStyle": {"color": "#b3cde3"},
+                        "lineStyle": {"color": "source", "opacity": 0.6},
+                    },
+                    {
+                        "depth": 2,
+                        "itemStyle": {"color": "#ccebc5"},
+                        "lineStyle": {"color": "source", "opacity": 0.6},
+                    },
+                    {
+                        "depth": 3,
+                        "itemStyle": {"color": "#decbe4"},
+                        "lineStyle": {"color": "source", "opacity": 0.6},
+                    },
+                ],
+                "lineStyle": {"curveness": 0.5},
+            }
+        ],
+    }
+    st_echarts(option,key="otro333", height="500px")
+
+
+    
+
     conn = st.connection("postgresql", type="sql")
 
 
@@ -102,44 +147,3 @@ def exporta_destino():
     data = json.loads(pp)
 
 
-    with open("./data/producto.json", "r") as f:
-        data = json.loads(f.read())
-
-
-
-    option = {
-        "title": {"text": "Sankey Diagram"},
-        "tooltip": {"trigger": "item", "triggerOn": "mousemove"},
-        "series": [
-            {
-                "type": "sankey",
-                "data": data["nodes"],
-                "links": data["links"],
-                "emphasis": {"focus": "adjacency"},
-                "levels": [
-                    {
-                        "depth": 0,
-                        "itemStyle": {"color": "#fbb4ae"},
-                        "lineStyle": {"color": "source", "opacity": 0.6},
-                    },
-                    {
-                        "depth": 1,
-                        "itemStyle": {"color": "#b3cde3"},
-                        "lineStyle": {"color": "source", "opacity": 0.6},
-                    },
-                    {
-                        "depth": 2,
-                        "itemStyle": {"color": "#ccebc5"},
-                        "lineStyle": {"color": "source", "opacity": 0.6},
-                    },
-                    {
-                        "depth": 3,
-                        "itemStyle": {"color": "#decbe4"},
-                        "lineStyle": {"color": "source", "opacity": 0.6},
-                    },
-                ],
-                "lineStyle": {"curveness": 0.5},
-            }
-        ],
-    }
-    st_echarts(option,key="otro333", height="500px")
