d3ef319fefc57597211b68c8b537f6c969064906
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-04-06 19:28:30 -0300

    Update exporta_pais1.py

--- exportaciones/exporta_pais1.py
+++ exportaciones/exporta_pais1.py
@@ -19,49 +19,7 @@ def exporta_destino():
         """
     st.markdown(streamlit_style, unsafe_allow_html=True)     
 
-    with open("./data/producto.json", "r") as f:
-        data = json.loads(f.read())
-
-
-
-    option = {
-        "title": {"text": "Sankey Diagram otro qu si anda"},
-        "tooltip": {"trigger": "item", "triggerOn": "mousemove"},
-        "series": [
-            {
-                "type": "sankey",
-                "data": data["nodes"],
-                "links": data["links"],
-                "emphasis": {"focus": "adjacency"},
-                "levels": [
-                    {
-                        "depth": 0,
-                        "itemStyle": {"color": "#fbb4ae"},
-                        "lineStyle": {"color": "source", "opacity": 0.6},
-                    },
-                    {
-                        "depth": 1,
-                        "itemStyle": {"color": "#b3cde3"},
-                        "lineStyle": {"color": "source", "opacity": 0.6},
-                    },
-                    {
-                        "depth": 2,
-                        "itemStyle": {"color": "#ccebc5"},
-                        "lineStyle": {"color": "source", "opacity": 0.6},
-                    },
-                    {
-                        "depth": 3,
-                        "itemStyle": {"color": "#decbe4"},
-                        "lineStyle": {"color": "source", "opacity": 0.6},
-                    },
-                ],
-                "lineStyle": {"curveness": 0.5},
-            }
-        ],
-    }
-    st_echarts(option,key="otro333", height="500px")
-
-
+  
     
 
     conn = st.connection("postgresql", type="sql")
@@ -121,7 +79,7 @@ def exporta_destino():
 
     df_anual = df_filtered.groupby(['pais'], as_index=False)[['fob', 'litros']].sum()
     df_variedad = df_filtered.groupby(['pais','variedad1'], as_index=False)[['fob', 'litros']].sum()
-    df_variedad = df_variedad.sort_values('fob')['litros'].head(10)
+    df_variedad = df_variedad.sort_values('fob').head(10)
     st.write(df_variedad)
     pais_list1 = sorted(df_variedad["pais"].dropna().unique(), reverse=True)
     var_list1 = sorted(df_variedad["variedad"].dropna().unique())
