64357d023646c6bf0311c318657a88c5ceddc52d
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-04-06 12:33:19 -0300

    Update exporta_pais1.py

--- exportaciones/exporta_pais1.py
+++ exportaciones/exporta_pais1.py
@@ -4,10 +4,7 @@ import numpy as np
 import json
 from streamlit_echarts import st_echarts
 from streamlit_echarts import JsCode
-from streamlit_echarts import st_pyecharts
-from pyecharts.charts import Bar
-from pyecharts import options as opts
-from pyecharts.charts import Line
+
 from datetime import datetime as dt
 
 def exporta_destino():
