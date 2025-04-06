45f77b90d534e0d4ce7cea8e4c3033f57ff88e91
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-04-06 12:05:34 -0300

    Update prueba.py

--- pages/prueba.py
+++ pages/prueba.py
@@ -82,9 +82,44 @@ st.html(
 tab1, tab2, tab3,tab4,tab5,tab6,tab7 = st.tabs(["Evolución", "Destinos", "Variedades","Color/Envase","Mosto Evolución","Mosto Destinos", "Mosto Productos"])
 with open("./data/producto.json", "r") as f:
     data = json.loads(f.read())
-st.write(data)
-st.write(data)
-st.write(data)
+
+option = {
+    "title": {"text": "Sankey Diagram"},
+    "tooltip": {"trigger": "item", "triggerOn": "mousemove"},
+    "series": [
+        {
+            "type": "sankey",
+            "data": data["nodes"],
+            "links": data["links"],
+            "emphasis": {"focus": "adjacency"},
+            "levels": [
+                {
+                    "depth": 0,
+                    "itemStyle": {"color": "#fbb4ae"},
+                    "lineStyle": {"color": "source", "opacity": 0.6},
+                },
+                {
+                    "depth": 1,
+                    "itemStyle": {"color": "#b3cde3"},
+                    "lineStyle": {"color": "source", "opacity": 0.6},
+                },
+                {
+                    "depth": 2,
+                    "itemStyle": {"color": "#ccebc5"},
+                    "lineStyle": {"color": "source", "opacity": 0.6},
+                },
+                {
+                    "depth": 3,
+                    "itemStyle": {"color": "#decbe4"},
+                    "lineStyle": {"color": "source", "opacity": 0.6},
+                },
+            ],
+            "lineStyle": {"curveness": 0.5},
+        }
+    ],
+}
+st_echarts(option, height="500px")
+
 option = {
     "title": {"text": "Sankey Diagram"},
     "tooltip": {"trigger": "item", "triggerOn": "mousemove"},
