41afadce8c3dd75188217bf627775a21d2d2814b
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-04-07 13:37:27 -0300

    Update exporta_pais.py

--- exportaciones/exporta_pais.py
+++ exportaciones/exporta_pais.py
@@ -335,11 +335,13 @@ def exporta_destino():
     #result3 = top_bottom_10.to_json(orient="records")
 
     df_var3 = df_var2.groupby(['pais'], as_index=False)[['fob', 'litros']].sum()
-    for index in range(len(df_var2)):
+    for index in range(len(df_var2) -1) :
         valor = top_bottom_10_pais['fob'].iloc[index]
         pais = top_bottom_10_pais['pais'].iloc[index]
+        valor2 = df_var2['fob'].iloc[index]
         st.write(valor)
         st.write(pais)
+        st.write(valor1)
         
     df_var2.drop(['litros'], axis='columns', inplace=True)
     df_var2 = df_var2.rename(columns={'pais': "source",'variedad1': "target",'fob': "value"})
