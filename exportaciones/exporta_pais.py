8d4888b5d4fc1e51ea8c135f343d5591f43f25ae
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-04-08 13:34:20 -0300

    Update exporta_pais.py

--- exportaciones/exporta_pais.py
+++ exportaciones/exporta_pais.py
@@ -469,7 +469,7 @@ def exporta_destino():
 
     df5 = df_variedad[~df_variedad['variedad1'].isin(var_listlts)]
     df5 = df5[~df5['pais'].isin(pais_listlts)]
-    Total = df5.groupby['litros'].sum()
+    Total = df5.['litros'].sum()
     st.write(Total)
     new_row = pd.Series({'fob': 1, 'pais': 'TOTAL PAISES', 'variedad1': 'OTROS','litros': tot, 'index' : len(df_varlts)})
     df_varlts = append_row(df_varlts, new_row) 
