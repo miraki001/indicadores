2bd0cd651412f389e497b3faa2c802d7c5e41fc0
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-04-08 16:30:59 -0300

    Update exporta_pais.py

--- exportaciones/exporta_pais.py
+++ exportaciones/exporta_pais.py
@@ -340,14 +340,14 @@ def exporta_destino():
                     "itemStyle": {"borderColor": "#fff"},
                     "levels": [
                         {"itemStyle": {"borderWidth": 0, "gapWidth": 5}},
-                        {"itemStyle": {"gapWidth": 1}},
+                        #{"itemStyle": {"gapWidth": 1}},
                       
                         {
                             "color": ["#942e38", '#aaa', "#269f3c", "#fbb4ae"],
                             #"color" :'#942e38',
                             "colorMappingBy": "value",
                             "colorSaturation": [0.35, 0.5],
-                            #"itemStyle": {"gapWidth": 1, "borderColorSaturation": 0.6},
+                            "itemStyle": {"gapWidth": 1, "borderColorSaturation": 0.6},
                         },
                     ],
                     "data": json_list,
