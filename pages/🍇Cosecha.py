3e36aca0942faf01569a38ebfea4623502eb554c
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-04-09 19:26:32 -0300

    Update 🍇Cosecha.py

--- pages/🍇Cosecha.py
+++ pages/🍇Cosecha.py
@@ -159,7 +159,7 @@ with tab1:
     #st.write(sql)
     #dv1 = conn.query(sql)
     #dv1 = conn.query('select anio,sum(sup) sup,count(*) cnt  from superficievariedad_m where (color = %1 or %1= '-1' group by anio order by anio ;', ttl="0")
-    df_anual = df_filtered.groupby(['anio'], as_index=False)[['sup', 'anio']].sum()
+    df_anual = df_filtered.groupby(['anio'], as_index=False)[['sup', 'cant']].sum()
     st.write(df_anual)
     #dv1['anio'] = dv1['anio'].astype(str)
 
