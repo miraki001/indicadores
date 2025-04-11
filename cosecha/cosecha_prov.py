1a600ddc4a0795553ce00c11e25c46baeb9c5a85
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-04-11 16:28:12 -0300

    Update cosecha_prov.py

--- cosecha/cosecha_prov.py
+++ cosecha/cosecha_prov.py
@@ -134,6 +134,7 @@ def cosecha_prov():
     totecon = df_anual['Consumo'].sum()
     totelab = df_anual['Secado'].sum()
     st.write(totelab)
+    st.write(df_anual['Elaboracion'])
     total = []
     #total.append(0)
     for index in range(len(df_anual)):
