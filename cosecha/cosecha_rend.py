bbed7f6b6eb1c010e8d147c11bc564d3b4bbc87b
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-04-14 08:54:25 -0300

    Update cosecha_rend.py

--- cosecha/cosecha_rend.py
+++ cosecha/cosecha_rend.py
@@ -240,3 +240,35 @@ def cosecha_rend():
 
     
     df_sorted = df_anual.sort_values(by='Provincia', ascending=True)
+
+    option = {
+        "tooltip": {
+            #"trigger": 'axis',
+            #"axisPointer": { "type": 'cross' },
+            "formatter": JsCode(
+                "function(info){var value=info.value;var treePathInfo=info.treePathInfo;var treePath=[];for(var i=1;i<treePathInfo.length;i+=1){treePath.push(treePathInfo[i].name)}return['<div class=\"tooltip-title\">'+treePath.join('/')+'</div>','Ventas Acumuladas: ' + value ].join('')};"
+            ).js_code,
+        },
+        "legend": {"data": ["litros","Pais"]},   
+        "series": [
+                {
+                    "name": "Ventas Totales",
+                    "type": "treemap",
+                    "visibleMin": 100,
+                    "label": {"show": True, "formatter": "{b}"},
+                    "itemStyle": {"borderColor": "#fff"},
+                    "levels": [
+                        {"itemStyle": {"borderWidth": 0, "gapWidth": 5}},
+                        {"itemStyle": {"gapWidth": 1}},
+                        {
+                            "colorSaturation": [0.35, 0.5],
+                            "itemStyle": {"gapWidth": 1, "borderColorSaturation": 0.6},
+                        },
+                    ],
+                    "data": json_list,
+                }
+        ]
+    }
+    st_echarts(
+        options=option,key="gauge2" + str(dt.now()), height="600px",
+    )
