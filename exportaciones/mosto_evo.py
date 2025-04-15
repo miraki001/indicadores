5a3f06630043a867ecdc1416b723bf81db866fbf
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-04-15 19:51:43 -0300

    Update mosto_evo.py

--- exportaciones/mosto_evo.py
+++ exportaciones/mosto_evo.py
@@ -142,7 +142,7 @@ def exporta_mosto_evo():
         with col1:
             with st.popover("Año"):
                 st.caption("Selecciona uno o más años de la lista")
-                año = st.multiselect("Año",   ["Todos"] + year_list, default=['Todos'],label_visibility="collapsed",help="Selecciona uno o más años")
+                año = st.multiselect("Año22",   ["Todos"] + year_list, default=['Todos'],label_visibility="collapsed",help="Selecciona uno o más años")
                 #anio = st.multiselect("Año22:", ["Todos"] + year_list, default=["Todos"])
                 año = [str(a) for a in año]  # Asegura que la selección sea string también
             
