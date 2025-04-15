5f7f57ae641afc9dd6665fffe6e9f9b35f9cd3b6
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-04-15 11:33:48 -0300

    Update exporta_pais.py

--- exportaciones/exporta_pais.py
+++ exportaciones/exporta_pais.py
@@ -235,6 +235,7 @@ def exporta_destino():
     
     df_varlts= df_varlts[df_varlts['pais'].isin(pais_listlts)]
     var_listlts.append("OTRAS")
+    st.write(var_listlts)
     var_listp = var_listlts
     var_listlts.append("TOTAL VARIEDAD")
     pais_listlts.append("OTROS")
