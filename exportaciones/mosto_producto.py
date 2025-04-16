d9381d2e3b4e6e68ec6a64d5b65f5f8dbfb01e2f
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-04-16 13:20:34 -0300

    Update mosto_producto.py

--- exportaciones/mosto_producto.py
+++ exportaciones/mosto_producto.py
@@ -75,7 +75,7 @@ def exporta_mosto_producto():
             return pd.DataFrame()
 
     # Cargar datos iniciales para llenar los filtros
-    QUERY_INICIAL = "select distinct anio,producto  from exportaciones2_m where producto = 'Mosto' and codigoproducto like '%CONCENTRADO%' ; "
+    QUERY_INICIAL = "select distinct anio,producto,pais  from exportaciones2_m where producto = 'Mosto' and codigoproducto like '%CONCENTRADO%' ; "
     df_filtros = cargar_datos(QUERY_INICIAL)
 
     if df_filtros.empty:
