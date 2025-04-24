feb69a6524ac4a451ef5de77796145550614a4f4
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-04-24 14:11:29 -0300

    Update desp_prov.py

--- despachos/desp_prov.py
+++ despachos/desp_prov.py
@@ -53,8 +53,7 @@ def despacho_prov(df_filtros,df):
     
     dv1['anio'] = dv1['anio'].astype(str)
 
-    df_filtered = dv1.copy()
-    
+
 
 
     with st.container(border=True):
@@ -86,157 +85,16 @@ def despacho_prov(df_filtros,df):
         Filtro = Filtro + ' Envases = ' +  str(envase) + ' '
             
 
-
-    #df_anual = df_filtered.groupby(['anio','prov','depto'], as_index=False)[['peso']].sum()
-    #df_anual = pd.pivot_table(df_filtered, values='peso', index=['prov'],
-    #                   columns=['destino'], aggfunc="sum")
-
-    df_anual = df_filtered.pivot_table(
-          index='prov', 
-          columns='destino',  
-          values=['peso'],
+    df_filtered = dv1.copy()
+    actual = dt.now().year -4 
+    df_filtered = df_filtered[df_filtered['anio'] > actual ]   
+    df_filtered = df_filtered.groupby(['anio'], as_index=False)[['litros']].sum()
+    litros = dv2.pivot_table(
+          index='provincia', 
+          columns='anio',  
+          values=['litros'],
           aggfunc='sum'
-    )
-    #st.write(df_anual)
-
-    df_anual.columns = df_anual.columns.droplevel(0)
-    df_anual = df_anual.reset_index().rename_axis(None, axis=1)
-    df_anual  = df_anual.fillna(0)
-
-    totelab = df_anual['Elaboracion'].sum()
-    totecon = df_anual['Consumo'].sum()
-    totesec = df_anual['Secado'].sum()
-    #st.write(totelab)
-    total = []
-    totco = []
-    totse = []
-    totto = []
-    #total.append(0)
-    for index in range(len(df_anual)):
-        total.append((  (df_anual['Elaboracion'].loc[index] / totelab) *100 ))
-        totco.append((  (df_anual['Consumo'].loc[index] / totecon) *100 ))
-        totse.append((  (df_anual['Secado'].loc[index] / totesec) *100 ))
-        totto.append((  (df_anual['Secado'].loc[index]  +  df_anual['Consumo'].loc[index] + df_anual['Elaboracion'].loc[index] ) ))
-    df_anual['Part. % Total Elab'] = total
-    df_anual['Part. % Total Cons'] = totco
-    df_anual['Part. % Total Sec'] = totse
-    df_anual['Total'] = totto
-
-    df_anual = df_anual.sort_index(axis = 1)
-    df_anual = df_anual.rename(columns={'prov': "Provincia"})
-    
-    df_sorted = df_anual.sort_values(by='Elaboracion', ascending=False)
-
-    styled_df = df_sorted.style.format(
-            {"Elaboracion": lambda x : '{:,.0f}'.format(x), 
-            "Consumo": lambda x : '{:,.0f}'.format(x), 
-            "Secado": lambda x : '{:,.0f}'.format(x),  
-            "Total": lambda x : '{:,.0f}'.format(x),  
-            "Part. % Total Elab": lambda x : '{:,.2f} %'.format(x),
-            "Part. % Total Cons": lambda x : '{:,.2f} %'.format(x),
-            "Part. % Total Sec": lambda x : '{:,.2f} %'.format(x), 
-            }
-            ,
-            thousands='.',
-            decimal=',',
-    )
-
-    column_orders =("Provincia", "Elaboracion","Part. % Total Elab","Consumo","Part. % Total Cons","Secado","Part. % Total Sec","Total")
-
-    if st.checkbox('Ver tabla Cosecha por Provincias'):
-        st.dataframe(styled_df,
-              column_config={
-                'Elaboracion': st.column_config.Column('Elaboracion'),
-                'Consumo': st.column_config.Column('Consumo'),
-                'Secado': st.column_config.Column('Secado'),
-                'Total': st.column_config.Column('Total'),
-                'Part. % Total Elab': st.column_config.Column('Part. % Total Elab'),        
-                'Part. % Total Cons': st.column_config.Column('Part. % Total Cons'),                          
-                'Part. % Total Sec': st.column_config.Column('Part. % Total Sec'),                   
-                },
-                column_order = column_orders,
-                width = 800,   
-                height = 400,
-                hide_index=True)
-    
-
-    dv = df_anual.groupby(['Provincia'], as_index=False)[['Total']].sum()
-    dv = dv.rename(columns={'Total': "value", 'Provincia': "name",})
-    json_list = json.loads(json.dumps(list(dv.T.to_dict().values()))) 
-    st.subheader('Cosecha por Provincias en Quintales')
-    #st.write(json_list)
-    st.caption(Filtro)
-
-    option = {
-        "tooltip": {
-            #"trigger": 'axis',
-            #"axisPointer": { "type": 'cross' },
-            "formatter": JsCode(
-                "function(info){var value=info.value;var treePathInfo=info.treePathInfo;var treePath=[];for(var i=1;i<treePathInfo.length;i+=1){treePath.push(treePathInfo[i].name)}return['<div class=\"tooltip-title\">'+treePath.join('/')+'</div>','Cosecha: ' + value ].join('')};"
-            ).js_code,
-        },
-        "legend": {"data": ["Qintales","Provincia"]},   
-        "series": [
-                {
-                    "name": "Cosecha",
-                    "type": "treemap",
-                    "visibleMin": 100,
-                    "label": {"show": True, "formatter": "{b}"},
-                    "itemStyle": {"borderColor": "#fff"},
-                    "levels": [
-                        {"itemStyle": {"borderWidth": 0, "gapWidth": 5}},
-                        {"itemStyle": {"gapWidth": 1}},
-                        {
-                            "colorSaturation": [0.35, 0.5],
-                            "itemStyle": {"gapWidth": 1, "borderColorSaturation": 0.6},
-                        },
-                    ],
-                    "data": json_list,
-                }
-        ]
-    }
-    st_echarts(
-        options=option,key="gauge2" + str(dt.now()), height="600px",
-    )
+    )  
+    st.write(litros)
 
 
-
-    dv = df_filtered.groupby(['prov','depto'], as_index=False)[['peso']].sum()
-    #dv = df_anual1
-    #st.write(dv)
-    dv = dv.rename(columns={'peso': "value", 'depto': "name",'prov': "id"})
-    json_list = json.loads(json.dumps(list(dv.T.to_dict().values()))) 
-    #st.write(json_list)
-
-    option = {
-        "tooltip": {
-            #"trigger": 'axis',
-            #"axisPointer": { "type": 'cross' },
-            "formatter": JsCode(
-                "function(info){var value=info.value;var treePathInfo=info.treePathInfo;var treePath=[];for(var i=1;i<treePathInfo.length;i+=1){treePath.push(treePathInfo[i].name)}return['<div class=\"tooltip-title\">'+treePath.join('/')+'</div>','Cosecha: ' + value ].join('')};"
-            ).js_code,
-        },
-        "legend": {"data": ["Qintales","Provincia"]},   
-        "series": [
-                {
-                    "name": "Cosecha",
-                    "type": "treemap",
-                    "visibleMin": 100,
-                    "label": {"show": True, "formatter": "{b}"},
-                    "itemStyle": {"borderColor": "#fff"},
-                    "colorMappingBy": 'id',
-                    "levels": [
-                        {"itemStyle": {"borderWidth": 0, "gapWidth": 5}},
-                        {"itemStyle": {"gapWidth": 1}},
-                        {
-                            "colorSaturation": [0.35, 0.5],
-                            "itemStyle": {"gapWidth": 1, "borderColorSaturation": 0.6},
-                        },
-                    ],
-                    "data": json_list,
-                }
-        ]
-    }
-    st_echarts(
-        options=option,key="gauge2" + str(dt.now()), height="600px",
-    )    
