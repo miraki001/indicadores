9138deddf228f1cdfc0d7b2463b6e1a0c88c0db2
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-04-25 15:11:08 -0300

    Update desp_color.py

--- despachos/desp_color.py
+++ despachos/desp_color.py
@@ -138,6 +138,7 @@ def despachos_color(df_filtros,df):
           aggfunc='sum'
     )  
     st.write(litros)
+    litros  = litros.fillna(0)
     litros.columns = litros.columns.droplevel(0)    
     for index in range(len(litros)):
         litros['Rosado'].loc[index] = litros['Rosado'].loc[index] +litros['Sin Dato'].loc[index]  
