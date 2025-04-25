ee06457a133e7fd8392c54b398afb30ecf1d9eaa
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-04-25 14:27:11 -0300

    Update desp_color.py

--- despachos/desp_color.py
+++ despachos/desp_color.py
@@ -7,7 +7,7 @@ from streamlit_echarts import JsCode
 
 from datetime import datetime as dt
 
-def despachos_prov(df_filtros,df):
+def despachos_color(df_filtros,df):
 
 
     hide_streamlit_style = """
@@ -71,7 +71,7 @@ def despachos_prov(df_filtros,df):
   
   
   # Listas de valores únicos para los filtros
-    #prov_list = sorted(df_filtros["provincia"].dropna().unique())
+    prov_list = sorted(df_filtros["provincia"].dropna().unique())
     envase_list = sorted(df_filtros["subgrupoenvase"].dropna().unique())
     producto_list = sorted(df_filtros["producto"].dropna().unique())
     #pais_list = sorted(df_filtros["pais"].dropna().unique())
@@ -88,7 +88,7 @@ def despachos_prov(df_filtros,df):
 
 
     with st.container(border=True):
-        col1, col2 =  st.columns([1, 1])  # Ajusta los tamaños de las columnas
+        col1, col2,col3 =  st.columns([1, 1,1])  # Ajusta los tamaños de las columnas
 
     # Columna 1: Filtro para Año
         with col1:
@@ -101,6 +101,11 @@ def despachos_prov(df_filtros,df):
             with st.popover("Envase"):
                 st.caption("Selecciona uno o más Envases de la lista")
                 envase = st.multiselect("Envasev",  ["Todos"] + envase_list, default=["Todos"],label_visibility="collapsed")
+
+        with col3:
+            with st.popover("Provincia"):
+                st.caption("Selecciona uno o más Provincias de la lista")
+                provincia = st.multiselect("Provincias12",  ["Todas"] + envase_list, default=["Todas"],label_visibility="collapsed")
     
     Filtro = 'Filtro = '    
     df_filtered = df.copy()    
@@ -115,6 +120,11 @@ def despachos_prov(df_filtros,df):
         if envase[0] != 'Todos':
             df_filtered = df_filtered[df_filtered['subgrupoenvase'].isin(color)]          
         Filtro = Filtro + ' Envases = ' +  str(envase) + ' '
+        
+    if provincia:
+        if provincia[0] != 'Todas':
+            df_filtered = df_filtered[df_filtered['provincia'].isin(provincia)]          
+        Filtro = Filtro + ' Provincias = ' +  str(provincia) + ' '
             
 
     #df_filtered = dv1.copy()
@@ -122,131 +132,10 @@ def despachos_prov(df_filtros,df):
     df_filtered = df_filtered[df_filtered['anio'] > actual ]   
     #df_filtered = df_filtered.groupby(['anio'], as_index=False)[['litros']].sum()
     litros = df_filtered.pivot_table(
-          index='provincia', 
-          columns='anio',  
+          index='anio', 
+          columns='color',  
           values=['litros'],
           aggfunc='sum'
     )  
-    litros  = litros.fillna(0)
-    anio1 = litros.columns[0]
-    anio2 = litros.columns[1]
-    anio3 = litros.columns[2]
-    anio4 = litros.columns[3]
-    #st.write(anio1)
-    totlitros1 = litros[anio1].sum()
-    totlitros2 = litros[anio2].sum()
-    totlitros3 = litros[anio3].sum()
-    totlitros4 = litros[anio4].sum()
-
-    tot1 = []
-    tot2 = []
-    tot3 = []
-    tot4 = []
-    df_anual = litros
-    df_anual.columns = df_anual.columns.droplevel(0)
-    anio1 = df_anual.columns[0]
-    anio2 = df_anual.columns[1]
-    anio3 = df_anual.columns[2]
-    anio4 = df_anual.columns[3]
-
-
-    df_anual = df_anual.reset_index().rename_axis(None, axis=1)
-    for index in range(len(df_anual)):
-        #if index > 0:
-            tot1.append((  (df_anual[int(anio1)].loc[index] / totlitros1 *100 )))
-            tot2.append((  (df_anual[int(anio2)].loc[index] / totlitros2 *100 )))
-            tot3.append((  (df_anual[anio3].loc[index] / totlitros3 *100 )))
-            tot4.append((  (df_anual[anio4].loc[index] / totlitros4 *100 )))
-    df_anual['Part. %' + str(anio1)  ] = tot1
-    df_anual['Part. % '+ str(anio2)  ] = tot2
-    df_anual['Part. % ' + str(anio3) ] = tot3
-    df_anual['Part. % ' + str(anio4) ] = tot4
-        
-
+    st.write(litros)
     
-    if st.checkbox('Ver Despachos por Provincias en forma de tabla'):
-        st.caption(Filtro)
-
-
-        df_anual = df_anual.rename(columns={'provincia': "Provincia"})
-
-        #df_anual = df_anual.sort_index(axis = 1)
-
-        styled_df = df_anual.style.format(
-            { anio1: lambda x : '{:,.0f}'.format(x), 
-              anio2: lambda x : '{:,.0f}'.format(x), 
-              anio3: lambda x : '{:,.0f}'.format(x), 
-              anio4: lambda x : '{:,.0f}'.format(x), 
-             'Part. % ' + str(anio1) : lambda x : '{:,.2f} %'.format(x),
-             'Part. % ' + str(anio2) : lambda x : '{:,.2f} %'.format(x),
-             'Part. % ' + str(anio3) : lambda x : '{:,.2f} %'.format(x),
-             'Part. % ' + str(anio4) : lambda x : '{:,.2f} %'.format(x),
-                                        }
-            ,
-            thousands='.',
-            decimal=',',
-        )
-        #st.write(styled_df)
-
-        column_orders =("Provincia", "2022", "Part. % 2022", "2023", "Part. % 2023" , "2024", "Part. % 2024", "2025", "Part. % 2025" )
-
-        st.dataframe(styled_df,
-              column_config={
-                'Provincia': st.column_config.Column('Provincia'),
-        
-                },
-                column_order = column_orders,
-                width = 1000,   
-                height = 800,
-                hide_index=True)
-
-    actual1 = dt.now().year -1
-    #st.write(actual1)
-    df_filtered = df_filtered[df_filtered['anio'] == actual1 ]  
-    #st.write(df_filtered)
-    df_filtered['anio'] = df_filtered['anio'].astype(str)
-    #df_filtered = df_filtered[df_filtered['anio'].isin(2024)]
-    df_anual = df_filtered.groupby(['provincia'], as_index=False)[['litros']].sum()
-    #st.write(df_anual)
-    df_anual = df_anual.rename(columns={'litros': "value", 'provincia': "name",})
-
-    json_list = json.loads(json.dumps(list(df_anual.T.to_dict().values()))) 
-    
-    #st.write(json_list)
-    
-    option = {
-        "tooltip": {
-            #"trigger": 'axis',
-            #"axisPointer": { "type": 'cross' },
-            "formatter": JsCode(
-                "function(info){var value=info.value;var treePathInfo=info.treePathInfo;var treePath=[];for(var i=1;i<treePathInfo.length;i+=1){treePath.push(treePathInfo[i].name)}return['<div class=\"tooltip-title\">'+treePath.join('/')+'</div>','Depachos Acumulados: ' + value ].join('')};"
-            ).js_code,
-        },
-        "title": {
-            "text": 'Despachos por Provincias en Litros',
-            "subtext": Filtro,
-        },        
-        #"subtitle": Filtro,
-        "legend": {"data": ["litros","provincia"]},   
-        "series": [
-                {
-                    "name": "Despachos Totales",
-                    "type": "treemap",
-                    "visibleMin": 100,
-                    "label": {"show": True, "formatter": "{b}"},
-                    "itemStyle": {"borderColor": "#fff"},
-                    "levels": [
-                        {"itemStyle": {"borderWidth": 0, "gapWidth": 5}},
-                        {"itemStyle": {"gapWidth": 1}},
-                        {
-                            "colorSaturation": [0.35, 0.5],
-                            "itemStyle": {"gapWidth": 1, "borderColorSaturation": 0.6},
-                        },
-                    ],
-                    "data": json_list,
-                }
-        ]
-    }
-    st_echarts(
-        options=option,key="gauge3322" + str(dt.now()), height="600px",
-    )
