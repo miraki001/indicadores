7a77e4e751ebd707d02ba606e63bc65058066e24
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-04-26 12:21:04 -0300

    Update desp_color.py

--- despachos/desp_color.py
+++ despachos/desp_color.py
@@ -256,6 +256,7 @@ def despachos_color(df_filtros,df):
         options=options, height="200px",
     )
 
+    #   ahora por producto
     
     tipo = df_filtered.pivot_table(
           index='anio', 
@@ -267,35 +268,137 @@ def despachos_color(df_filtros,df):
     tipo.columns = tipo.columns.droplevel(0)   
     tipo = tipo.reset_index()
     st.write(tipo)
+    for index in range(len(tipo)):
+        tipo['Otros Vinos'].loc[index] = tipo['Otros Vinos'].loc[index] +tipo['Otros'].loc[index]  
  
     tot1 = []
     for index in range(len(tipo)):
-        tot1.append((  (litros['Rosado'].loc[index]  +  litros['Blanco'].loc[index] + litros['Tinto'].loc[index]  )))
-    litros['Total'] = tot1        
+        tot1.append((  (tipo['Espumantes'].loc[index]  +  tipo['Gasificados'].loc[index] + tipo['Otros Vinos'].loc[index] + tipo['Vino Varietal'].loc[index] + tipo['Vinos sin Mencion'].loc[index]   )))
+    tipo['Total'] = tot1        
     tot1 = []
     tot2 = []
     tot3 = []
-    for index in range(len(litros)):
+    tot4 = []
+    tot5 = []
+    for index in range(len(tipo)):
         #if index > 0:
-            tot1.append((  (litros['Rosado'].loc[index] / litros['Total'].loc[index]  ) *100 ))
-            tot2.append((  (litros['Blanco'].loc[index] / litros['Total'].loc[index]  *100 )))
-            tot3.append((  (litros['Tinto'].loc[index] / litros['Total'].loc[index]  * 100 ) ) )
-    litros['Part. % Rosado'] = tot1
-    litros['Part. % Blanco'] = tot2
-    litros['Part. % Tinto'] = tot3    
-    #st.write(litros)
-    litros = litros.rename(columns={'anio': "Año"})
+            tot1.append((  (tipo['Espumantes'].loc[index] / tipo['Total'].loc[index]  ) *100 ))
+            tot2.append((  (tipo['Gasificados'].loc[index] / tipo['Total'].loc[index]  *100 )))
+            tot3.append((  (tipo['Otros Vinos'].loc[index] / tipo['Total'].loc[index]  * 100 ) ) )
+            tot4.append((  (tipo['Vino Varietal'].loc[index] / tipo['Total'].loc[index]  * 100 ) ) )
+            tot5.append((  (tipo['Vinos sin Mencion'].loc[index] / tipo['Total'].loc[index]  * 100 ) ) )
+    tipo['Part. % Esp.'] = tot1
+    tipo['Part. % Gas.'] = tot2
+    tipo['Part. % Otros'] = tot3    
+    tipo['Part. % Var.'] = tot4    
+    tipo['Part. % Sin'] = tot5    
+    st.write(tipo)
+    tipo = tipo.rename(columns={'anio': "Año"})
 
     styled_df = litros.style.format(
-            {"Rosado": lambda x : '{:,.0f}'.format(x), 
-            "Blanco": lambda x : '{:,.0f}'.format(x),
-            "Tinto": lambda x : '{:,.0f}'.format(x),
+            {"Espumantes": lambda x : '{:,.0f}'.format(x), 
+            "Gasificados": lambda x : '{:,.0f}'.format(x),
+            "Otros Vinos": lambda x : '{:,.0f}'.format(x),
+            "Vino Varietal": lambda x : '{:,.0f}'.format(x),
+            "Vinos sin Mencion": lambda x : '{:,.0f}'.format(x),
             "Total": lambda x : '{:,.0f}'.format(x),
-            "Part. % Rosado": lambda x : '{:,.2f} %'.format(x),
-            "Part. % Blanco": lambda x : '{:,.2f} %'.format(x),
-            "Part. % Tinto": lambda x : '{:,.2f} %'.format(x),
+            "Part. % Esp.": lambda x : '{:,.2f} %'.format(x),
+            "Part. % Gas.": lambda x : '{:,.2f} %'.format(x),
+            "Part. % Otros": lambda x : '{:,.2f} %'.format(x),
+            "Part. % Var.": lambda x : '{:,.2f} %'.format(x),
+            "Part. % Sin": lambda x : '{:,.2f} %'.format(x),
                                         }
             ,
             thousands='.',
             decimal=',',
+    )
+    if st.checkbox('Ver Depachos por Producto en forma de tabla'):
+        column_orders =("Año", "Espumantes", "Part. % Esp.", "Gasificados", "Part. % Gas.", "Otros Vinos" , "Part. % Otros", "Vino Varietal","Part. % Var.","Vinos sin Mencion","Part. % Sin")
+       
+        st.dataframe(styled_df,
+              column_config={
+                'Año': st.column_config.Column('Año'),                  
+                'Espumantes': st.column_config.Column('Espumantes'),
+                'Gasificados': st.column_config.Column('Gasificados'),
+                'Otros Vinos': st.column_config.Column('Otros Vinos'),
+                'Vino Varietal': st.column_config.Column('Vino Varietal'),
+                'Vinos sin Mencion"': st.column_config.Column('Vinos sin Mencion"'),
+                'Total': st.column_config.Column('Total'),
+                'Part. % Esp.': st.column_config.Column('Part. % Esp.'),
+                'Part. % Gas.': st.column_config.Column('Part. % Gas.'),
+                'Part. % Otros': st.column_config.Column('Part. % Otros'),
+                'Part. % Var.': st.column_config.Column('Part. % Var.'),
+                'Part. % Sin': st.column_config.Column('Part. % Sin'),
+        
+                },
+                column_order = column_orders,     
+                width = 1000,   
+                height = 800,
+                hide_index=True)
+    option = {
+        "tooltip": {
+        "trigger": 'axis',
+        "axisPointer": { "type": 'cross' }
+        },
+        "legend": {},    
+        "xAxis": {
+            "type": "category",
+            "data": tipo['Año'].to_list(),
+        },
+        "yAxis": {"type": "value"},
+        "series": [{"data": tipo['Espumantes'].to_list(), "type": "line", "name": 'Espumantes',"color":'#C92488', },
+                   {"data": tipo['Gasificados'].to_list(), "type": "line", "name": 'Gasificados',"color":'#F49F82',},
+                   {"data": tipo['Otros Vinos'].to_list(), "type": "line", "name": 'Otros Vinos',"color":'#604994',},
+                   {"data": tipo['Vino Varietal'].to_list(), "type": "line", "name": 'Vino Varietal',"color":'#604994',},
+                   {"data": tipo['Vinos sin Mencion'].to_list(), "type": "line", "name": 'Vinos sin Mencion',"color":'#604994',},
+               ]
+      }
+    st_echarts(
+         options=option, height="400px" ,
+    )
+    actual1 = dt.now().year -1
+    #st.write(actual1)
+    tipo = tipo[litros['Año'] == actual1 ]  
+    espumantes = int(tipo['Espumantes'])
+    gasificados = int(tipo['Gasificados'])
+    otros = int(tipo['Otros Vinos'])
+    varietal = int(tipo['Vino Varietal'])
+    comunes = int(tipo['Vinos sin Mencion'])
+    st.write(blanco)
+
+    options = {
+            "color": [
+            '#C92488',
+            '#F49F82',
+            '#604994',
+
+            ],        
+            "title": {"text": "", "left": "center"},
+            "subtitle":{"text": ""},
+            "tooltip": {"trigger": "item"},
+            "legend": {"orient": "vertical", "left": "left",},
+            "series": [
+            {
+                "name": "Hl",
+                "type": "pie",
+                "radius": "90%",
+                "data": [
+                    {"value": espumantes, "name": "Espumantes"},
+                    {"value": gasificados , "name": "Gasificados"},
+                    {"value": otros , "name": "Otros Vinos"},
+                    {"value": varietal , "name": "Vino Varietal"},
+                    {"value": comunes , "name": "Vino sin Mencion"},
+                ],
+                "emphasis": {
+                    "itemStyle": {
+                        "shadowBlur": 10,
+                        "shadowOffsetX": 0,
+                        "shadowColor": "rgba(10, 0, 0, 0.5)",
+                    }
+                },
+            }
+            ],
+    }
+    st_echarts(
+        options=options, height="200px",
     )
