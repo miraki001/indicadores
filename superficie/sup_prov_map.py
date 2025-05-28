dad14b5d36665772438c73537d59c6e97ea59a6c
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-05-28 13:47:59 -0300

    Update sup_prov_map.py

--- superficie/sup_prov_map.py
+++ superficie/sup_prov_map.py
@@ -105,7 +105,7 @@ def prov_map(df):
   choropleth.geojson.add_to(map)  
   for feature in choroplet.geojson.data['features']:
       prov1 = feature['properties']['name']
-      feature['properties']['superficie'] = 'Superficie: ' + str(df.loc[prov1,'sup'][0] if prov1 list(df.index) else 'N/A')
+      feature['properties']['superficie'] = 'Superficie: ' + str('{:,}'.format(df.loc[prov1,'sup'][0]) if prov1 list(df.index) else 'N/A')
   choropleth.geojson.add_child(
       folium.features.GeoJsonTooltip(['name',],labels=False)
   )
