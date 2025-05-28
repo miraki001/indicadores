58e58f7c41d7d90ee24a216cb7ccb6dc1b934cbb
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-05-28 14:39:23 -0300

    Update sup_prov_map.py

--- superficie/sup_prov_map.py
+++ superficie/sup_prov_map.py
@@ -124,7 +124,7 @@ def prov_map(df):
   for feature in choropleth.geojson.data['features']:
       prov1 = feature['properties']['name']
       st.write(prov1)
-      pp = df_indexed.loc[prov1, 'sup'][0]
+      #pp = df_indexed.loc[prov1, 'sup'][0]
       #st.write(pp)
       feature['properties']['superficie'] = 'Superficie: ' +  '{:,}'.format(df_indexed.loc[prov1, 'sup'][0]) if prov1 in list(df_indexed.index) else ''
   choropleth.geojson.add_child(
