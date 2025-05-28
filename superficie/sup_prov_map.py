4b1a49642da792e996ad9933c8652e3e701973c0
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-05-28 14:26:36 -0300

    Update sup_prov_map.py

--- superficie/sup_prov_map.py
+++ superficie/sup_prov_map.py
@@ -110,7 +110,7 @@ def prov_map(df):
       highlight=True,
   )
 
-  pp = df_indexed.loc['Entre Rios', 'sup'][0]
+  pp = df_indexed.loc['Salta', 'sup'][0]
   st.write(pp)    
   choropleth.geojson.add_to(map)  
   for feature in choropleth.geojson.data['features']:
