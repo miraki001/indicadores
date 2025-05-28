4a683c8d8cea74e3e4a16a20a327186409ec3604
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-05-28 16:19:12 -0300

    Update sup_prov_map.py

--- superficie/sup_prov_map.py
+++ superficie/sup_prov_map.py
@@ -101,7 +101,7 @@ def prov_map(df):
     
   df_indexed = df.set_index('provincia')    
 
-  max_value = df[sup].max()
+  max_value = df['sup'].max()
   hex_codes = ['#0b0405', '#28192e', '#3b2e5d', '#40498e', '#366a9f', '#348ba6', '#38aaac', '#55caad', '#a1dfb9', '#def5e5']  
   custom_colour_map = cm.StepColormap(colors = hex_codes, vmin = 0, vmax = max_value, tick_labels=[1, 100, 1000, 10000, max_value])    
     
