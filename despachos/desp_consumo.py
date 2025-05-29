2b1f29af2758bc4ec11b8b528b46a98664a9853a
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-05-29 14:54:04 -0300

    Update desp_consumo.py

--- despachos/desp_consumo.py
+++ despachos/desp_consumo.py
@@ -205,7 +205,7 @@ def despachos_consumo():
 
     dv3 = dv3.set_index(['anio','mes','canal'])   
     #dv3 = dv3.reset_index().rename_axis(None, axis=1)  
-    dv3.insert(loc=0,  value=np.arange(len(dv3)))
+    dv3 = dv3.assign(row_number=range(len(dv3)))
     #dv3 = dv3.set_index(['anio','mes','canal']) 
     st.write(dv3)
 
