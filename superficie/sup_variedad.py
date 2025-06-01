b7f079f8f7e1bdd64b7261991ce41a0bd112d5ce
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-06-01 12:04:33 -0300

    Update sup_variedad.py

--- superficie/sup_variedad.py
+++ superficie/sup_variedad.py
@@ -274,6 +274,6 @@ def sup_variedad():
     st.altair_chart(chart, theme="streamlit", use_container_width= True)
     fig = px.scatter(df, x="provincia", y="variedad",
 	         size="sup", color="variedad",
-                 hover_name="provincia", log_x=True, size_max=60)
+                 hover_name="variedad", log_x=True, size_max=60)
     #fig.show()
     event = st.plotly_chart(fig, key="iris", on_select="rerun")
