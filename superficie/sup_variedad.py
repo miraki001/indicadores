347f17ae4b42a32acee71e264c43e0b14432b922
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-06-02 11:19:20 -0300

    Update sup_variedad.py

--- superficie/sup_variedad.py
+++ superficie/sup_variedad.py
@@ -279,7 +279,7 @@ def sup_variedad():
 	         size="sup", color="variedad",
                  hover_name="provincia", log_x=True, size_max=200)
     #fig.show()
-    #fig.update_traces(marker_size=40)	
+    fig.update_traces(marker_size=20)	
     #fig.update_layout(scattermode="group")
     event = st.plotly_chart(fig, key="provincia")
     st.write(df)
