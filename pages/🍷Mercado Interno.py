8ab76a84c38bb3440bc62db438309c0a107a38a2
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-06-06 17:10:59 -0300

    Update 🍷Mercado Interno.py

--- pages/🍷Mercado Interno.py
+++ pages/🍷Mercado Interno.py
@@ -311,6 +311,7 @@ with tab1:
   fig = make_subplots(specs=[[{"secondary_y": True}]])  
   for y in df2.anio.unique():
     dfy = df2[df2.anio == y]
+    st.write(dfy)
     for indx in range(len(dfy['mes'])):
       hovertexts.append('({x},{y})'.format(x='Mes : ' +  dfy['mes'][indx], y= 'Hl : ' +  str(dfy['litros'][indx])))
 
