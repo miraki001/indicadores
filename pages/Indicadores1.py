14d88b67cc4e0e92a71db48c22e25983e5671cae
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-06-10 15:03:46 -0300

    Update Indicadores1.py

--- pages/Indicadores1.py
+++ pages/Indicadores1.py
@@ -83,10 +83,12 @@ with tab1:
       dva = dv1[dv1['anio'] == actual ]
       dvo = dv1[dv1['anio'] == anterior ]
       st.write(dva)
+      st.write(max(dva['mes'])
       
      
    with col[1]:
       st.write(dvo)
+      st.write(max(dvo['mes'])
 with tab2:
   st.write('vacio')
   
