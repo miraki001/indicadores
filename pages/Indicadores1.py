bdf6f99c8bca60a072b8e3b64b137b621b833ec7
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-06-11 15:04:10 -0300

    Update Indicadores1.py

--- pages/Indicadores1.py
+++ pages/Indicadores1.py
@@ -78,7 +78,7 @@ locale.setlocale(locale.LC_ALL, "de_DE.UTF-8")
 
 def _format_with_thousands_commas(val): 
   #val = round(val,0)
-  return f'{val:12n}' 
+  return f'{val:.12n}' 
 
 def _format_as_percentage(val, prec=0): 
   return f'{val:.{prec}%}' 
