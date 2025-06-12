a135f63a834823d9f0fd024b58f8f162b72dfa2d
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-06-12 16:15:42 -0300

    Update ind_exportaciones.py

--- kpi/ind_exportaciones.py
+++ kpi/ind_exportaciones.py
@@ -98,8 +98,8 @@ def ind_exportaciones(dva):
             "series": [
                 {"data": dv1['litros'].tolist(), "type": "bar", "name": actual,"yAxisIndex": 1, "color":'#F9C8B4'  },
                 {"data": dv2['litros'].tolist(), "type": "bar", "name": anterior,"yAxisIndex": 1, "color":'#FCE2D6'  },
-                {"data": dv1['fob'].tolist(), "type": "line", "name": 'u$s', "yAxisIndex": 2,  "color":'#C92488'},
-                {"data": dv2['fob'].tolist(), "type": "line", "name": 'u$s', "yAxisIndex": 2,  "color":'#604994'},
+                {"data": dv1['fob'].tolist(), "type": "line", "name": actual, "yAxisIndex": 2,  "color":'#C92488'},
+                {"data": dv2['fob'].tolist(), "type": "line", "name": 'u$s' + anterior, "yAxisIndex": 2,  "color":'#604994'},
                 
             ],
     }
