1bd9531c4c0273dc2810ee68bbc2b38ded985ad0
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-06-13 18:05:04 -0300

    Update Indicadores1.py

--- pages/Indicadores1.py
+++ pages/Indicadores1.py
@@ -280,7 +280,7 @@ with tab1:
 
       dvoa = dv1[dv1['anio'] == anterior-1 ]
       dvoa = dvoa[dvoa['mes']  <= mes]
-      valoa = dvoa['litros'].sum()
+      valoa = dvoa['litros'].sum()/100
       deltaoa = valo/valoa
       st.write(valoa)
       st.write(valo)
