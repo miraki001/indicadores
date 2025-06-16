835e5aae0f83da407e608d7fddbfd0e99cf2a627
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-06-16 09:25:35 -0300

    Update 🍇Cosecha.py

--- pages/🍇Cosecha.py
+++ pages/🍇Cosecha.py
@@ -199,6 +199,7 @@ with tab1:
         st.write(df_filtered)
 
         st.error("No se encontraron datos en la base de datos.")
+        st.close()
         #st.stop()  
   
     st.header("Cantidad de Viñedos")
