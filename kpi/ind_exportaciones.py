badcbd18d669bcefb4d08a6745a76ada9477b262
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-06-19 13:26:32 -0300

    Update ind_exportaciones.py

--- kpi/ind_exportaciones.py
+++ kpi/ind_exportaciones.py
@@ -304,7 +304,7 @@ def ind_exportaciones(dva):
           }
       }        
       )
-      streamlit_kpi(key="one",height=100,title='ver',value=100,icon='fa-globe',progressValue=100,unit=1000,animate=True,animateDuration=1000,
+      streamlit_kpi(key="one",height=100,title='ver',value=100,icon='fa-globe',progressValue=100,unit=1000,animate=True,animateDuration=100000,
                 showProgress=True,iconTop=1,showIcon=True,
                 iconLeft=0,iconOpacity=0,iconColor=0,
                 backgroundColor='#f3f3f3',titleColor='black',valueColor='black',
