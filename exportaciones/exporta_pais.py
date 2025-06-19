f9207fa27a1ac1eeaf2cfea7cb8c5f947e4e2b19
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-06-19 17:55:55 -0300

    Update exporta_pais.py

--- exportaciones/exporta_pais.py
+++ exportaciones/exporta_pais.py
@@ -713,7 +713,10 @@ def exporta_destino():
     st.write('aca')
     st.write(df_pivot)
     anios = sorted([col for col in df_pivot.columns if col != 'pais'])
-    st.write(anios)
+    #st.write(anios)
+    
+    df_pct = df_pivot[['anios','pais']].pct_change(axis=1)
+    st.write(df_pct)
     df_pct = df_pivot[anios].pct_change(axis=1)
     st.write('aca se pierden los años')
     st.write(df_pct)
