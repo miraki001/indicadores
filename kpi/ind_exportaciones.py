0c0e12c5243a49b1adb3af50ea30282568069c45
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-06-19 14:26:34 -0300

    Update ind_exportaciones.py

--- kpi/ind_exportaciones.py
+++ kpi/ind_exportaciones.py
@@ -324,7 +324,7 @@ def ind_exportaciones(dva):
       #  x = dv1['anio'],
       #  y = dv1['litros']))
       fig.add_trace(
-        go.Scatter(x=dv1.anio, y=dv1.mes1, mode="lines")
+        go.Scatter(x=dv1.mes1, y=dv1.litros, mode="lines")
       )    
 
       fig.show()
