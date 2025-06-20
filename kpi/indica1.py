14e8a0a825ff76ced6f86996fe51c47814f7fd34
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-06-20 10:44:07 -0300

    Update indica1.py

--- kpi/indica1.py
+++ kpi/indica1.py
@@ -26,40 +26,51 @@ def _format_as_percentage(val, prec=0):
 def indica1(dv1):
     actual = dt.now().year  
     anterior = dt.now().year -1 
+    dva = dv1[dv1['anio'] == actual ]
+    dvo = dv1[dv1['anio'] == anterior ]
+    dvoa = dv1[dv1['anio'] == anterior-1 ]
+    mes = max(dva['mes'])
+    dvam = dva[dv1['mes'] == mes ]
+    dvo = dvo[dvo['mes']  <= mes]
+    dvoa = dvoa[dvoa['mes']  <= mes]
+    vala = dva['litros'].sum()
+    valo = dvo['litros'].sum()
+    valoa = dvoa['litros'].sum()
+    #st.write(valoa)
+    #st.write(valo)
+    deltaoa = valo/valoa
+    deltaoa = (deltaoa -1)*100
+    #if deltaoa < 1:
+    #  deltaoa = (1- deltaoa) * -1
+     
+    deltao = valo/vala
+    if deltao < 1:
+        deltao = (1- deltao) * -1
+    deltaa = vala/valo
+    deltaa = (deltaa - 1)*100
+    valoro = str(_format_with_thousands_commas(valo)) 
+    valora = str(_format_with_thousands_commas(vala)) 
+    delta1 = str(_format_as_percentage(deltaoa,2))
+    delta2 = str(_format_as_percentage(deltaa,2))
+    mes2 = max(dva['mes1'])
     col = st.columns((4.5, 4.5), gap='small')
     with col[0]:
-      dva = dv1[dv1['anio'] == actual ]
-      dvo = dv1[dv1['anio'] == anterior ]
-      dvoa = dv1[dv1['anio'] == anterior-1 ]
-      mes = max(dva['mes'])
-      dvam = dva[dv1['mes'] == mes ]
-      dvo = dvo[dvo['mes']  <= mes]
-      dvoa = dvoa[dvoa['mes']  <= mes]
-      vala = dva['litros'].sum()
-      valo = dvo['litros'].sum()
-      valoa = dvoa['litros'].sum()
-      #st.write(valoa)
-      #st.write(valo)
-      deltaoa = valo/valoa
-      deltaoa = (deltaoa -1)*100
-      #if deltaoa < 1:
-      #  deltaoa = (1- deltaoa) * -1
-     
-      deltao = valo/vala
-      if deltao < 1:
-        deltao = (1- deltao) * -1
-      deltaa = vala/valo
-      deltaa = (deltaa - 1)*100
-      #if deltaa < 1:
-      #  deltaa = (1- deltaa) * -1
-      valoro = str(_format_with_thousands_commas(valo)) 
-      valora = str(_format_with_thousands_commas(vala)) 
-      delta1 = str(_format_as_percentage(deltaoa,2))
-      mes2 = max(dva['mes1'])
       product_card(
             product_name=delta1 + "%",
             description='Despachos 2024', 
             price=valoro,       
+            product_image='https://enolife.com.ar/es/wp-content/uploads/2025/06/Imagen1-10-1024x440.jpg', 
+            picture_position="left",
+            image_aspect_ratio="3/2",
+            button_text=None,   
+            on_button_click=lambda: handle_card_click("Clickable Card Area"),
+            key="core_name_only"
+      )
+    with col[1]:
+      product_card(
+            product_name=delta2 + "%",
+            description='Despachos 2025', 
+            price=valora,       
             product_image='https://enolife.com.ar/es/wp-content/uploads/2025/06/Imagen1-10-1024x440.jpg', 
             picture_position="left",
             image_aspect_ratio="3/2",
