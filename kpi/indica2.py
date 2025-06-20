4020238d016d62abe85a6bca9656ba96fb144c72
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-06-20 11:36:06 -0300

    Update indica2.py

--- kpi/indica2.py
+++ kpi/indica2.py
@@ -65,12 +65,12 @@ def indica2(dv1):
       value = valo,
       domain = {'x': [0, 1], 'y': [0, 1]},
       title = {'text': "Despachos 2024"}))
-      fig1.add_trace(go.Scatter(
-        x = dvo['mes1'],
-        y = dvo['litros']))
-      #fig.add_trace(
-      #  go.add_bar(x=dv1.mes1, y=dv1.litros)
-      #)    
+      #fig1.add_trace(go.Scatter(
+      #  x = dvo['mes1'],
+      #  y = dvo['litros']))
+      fig.add_trace(
+        go.line(x=dvo.mes1, y=dvo.litros)
+      )    
       #fig1.add_bar(x = dvoa.mes1,  y = dvoa.litros)
       #fig.update_layout(paper_bgcolor = "lightgray")
       fig1.show()
