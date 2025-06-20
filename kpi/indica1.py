cbf56374c34f5be012674a8c2ec261841ea8cfd3
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-06-20 10:04:30 -0300

    Update indica1.py

--- kpi/indica1.py
+++ kpi/indica1.py
@@ -13,8 +13,8 @@ from streamlit_product_card import product_card
 
 def indica1(dv1):
     product_card(
-        product_name="Despachos",
-        description='2024', 
+        product_name="2.3%",
+        description='Despachos 2024', 
         price=345.345,       
         product_image='https://enolife.com.ar/es/wp-content/uploads/2025/06/Imagen1-10-1024x440.jpg', 
         picture_position="left",
