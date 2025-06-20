72600ec5c760d8ddd1b45bdcdc4a343c4134857d
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-06-20 09:49:15 -0300

    Update indica1.py

--- kpi/indica1.py
+++ kpi/indica1.py
@@ -5,27 +5,18 @@ import json
 from datetime import datetime as dt
 import locale
 from script.exportaciones import mosto_registro_mensual
-from streamlit_card import card
 from streamlit_kpi import streamlit_kpi
+from streamlit_product_card import product_card 
 
 
 
 
 def indica1(dv1):
-
-  despachos = card(
-        title="Despachos",
-        text="Some description",
-        image="http://placekitten.com/200/300",
-        styles={
-          "card": {
-              "width": "200px",
-              "height": "200px",
-              "border-radius": "10px",
-              "box-shadow": "0 0 10px rgba(0,0,0,0.5)",
-          },
-          "text": {
-              "font-family": "serif",
-          }
-      }        
-  )
+    product_card(
+        product_name="Product Name Only",
+        description=None, 
+        price=None,       
+        product_image=None, 
+        button_text=None,   
+        key="core_name_only"
+    )
