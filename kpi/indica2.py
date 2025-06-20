541c2daf161517cc1d3a76b093e19f8e0cfc2548
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-06-20 12:17:08 -0300

    Update indica2.py

--- kpi/indica2.py
+++ kpi/indica2.py
@@ -61,7 +61,7 @@ def indica2(dv1):
       fig1 = go.Figure(go.Indicator(
       mode = "number+delta",
       #gauge = {'shape': "bullet"},
-      delta = {'reference': valoa, 'relative': True},
+      delta = {'reference': valoa, 'relative': True, 'valueformat': ".2f"},
       value = valo,
       domain = {'x': [0, 1], 'y': [0, 1]},
       title = {'text': "Despachos 2024"}))
