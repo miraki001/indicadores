798eea3418c32b47cfcb6a6770cc0b8e37f395e6
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-06-26 16:38:30 -0300

    Update Indicadores1.py

--- pages/Indicadores1.py
+++ pages/Indicadores1.py
@@ -314,10 +314,10 @@ with tab1:
             picture_position="left",
             image_aspect_ratio="16/9",
             image_object_fit="contain",
-            font_url="https://fonts.googleapis.com/css2?family=Old+Standard+TT:wght@400;700&family=Roboto+Condensed:wght@400&display=swap",
+            font_url="https://fonts.googleapis.com/css2?family=Roboto+Regular 400:wght@400;700&family=Roboto+Condensed:wght@400&display=swap",
             styles={
 
-                "title": {"font-family": "'Roboto Condensed', serif", "font-weight": "700", "font-size": "0.8em", "color": "#E31E24" },
+                "title": {"font-family": "'Roboto Regular 400', serif", "font-weight": "700", "font-size": "0.8em", "color": "#E31E24" },
                 "text": {"font-family": "'Roboto Condensed', serif", "line-height": "1.6", "font-size": "1.1em", "color": "#5d4037"},
                 "price": { "background-color": "#E3E5E9","font-family": "'Roboto Condensed', serif", "line-height": "1.6", "font-size": "1.6em", "color": bgcolor_positive_or_negative(deltaa)},
 
@@ -374,10 +374,10 @@ with tab1:
             picture_position="left",
             image_aspect_ratio="16/9",
             image_object_fit="contain",
-            font_url="https://fonts.googleapis.com/css2?family=Old+Standard+TT:wght@400;700&family=Roboto+Condensed:wght@400&display=swap",
+            font_url="https://fonts.googleapis.com/css2?family=Roboto+Regular 400:wght@400;700&family=Roboto+Condensed:wght@400&display=swap",
             styles={
 
-                "title": {"font-family": "'Roboto Condensed', serif", "text-transform": "uppercase",   "font-weight": "700", "font-size": "0.8em", "color": "#E31E24" },
+                "title": {"font-family": "'Roboto Regular 400', serif", "text-transform": "uppercase",   "font-weight": "700", "font-size": "0.8em", "color": "#E31E24" },
                 "text": {"font-family": "'Roboto Condensed', serif", "line-height": "1.6", "font-size": "1.1em", "color": "#5d4037"},
                 "price": { "background-color": "#E3E5E9","font-family": "'Roboto Condensed', serif", "line-height": "1.6", "font-size": "1.5em", "color": bgcolor_positive_or_negative(deltaa)},
 
@@ -447,10 +447,10 @@ with tab1:
             picture_position="left",
             image_aspect_ratio="16/9",
             image_object_fit="contain",
-            font_url="https://fonts.googleapis.com/css2?family=Old+Standard+TT:wght@400;700&family=Roboto+Condensed:wght@400&display=swap",
+            font_url="https://fonts.googleapis.com/css2?family=Roboto+Regular 400:wght@400;700&family=Roboto+Condensed:wght@400&display=swap",
             styles={
 
-                "title": {"font-family": "'Roboto Condensed', serif", "text-transform": "uppercase",   "font-weight": "700", "font-size": "0.8em", "color": "#E31E24" },
+                "title": {"font-family": "'Roboto Regular 400', serif", "text-transform": "uppercase",   "font-weight": "700", "font-size": "0.8em", "color": "#E31E24" },
                 "text": {"font-family": "'Roboto Condensed', serif", "line-height": "1.6", "font-size": "1.1em", "color": "#5d4037"},
                 "price": { "background-color": "#E3E5E9","font-family": "'Roboto Condensed', serif", "line-height": "1.6", "font-size": "1.5em", "color": bgcolor_positive_or_negative(deltaa)},
 
@@ -511,10 +511,10 @@ with tab1:
             picture_position="left",
             image_aspect_ratio="16/9",
             image_object_fit="contain",
-            font_url="https://fonts.googleapis.com/css2?family=Old+Standard+TT:wght@400;700&family=Roboto+Condensed:wght@400&display=swap",
+            font_url="https://fonts.googleapis.com/css2?family=Roboto+Regular 400:wght@400;700&family=Roboto+Condensed:wght@400&display=swap",
             styles={
 
-                "title": {"font-family": "'Roboto Condensed', serif", "text-transform": "uppercase",   "font-weight": "700", "font-size": "0.8em", "color": "#E31E24" },
+                "title": {"font-family": "'Roboto Regular 400', serif", "text-transform": "uppercase",   "font-weight": "700", "font-size": "0.8em", "color": "#E31E24" },
                 "text": {"font-family": "'Roboto Condensed', serif", "line-height": "1.6", "font-size": "1.1em", "color": "#5d4037"},
                 "price": { "background-color": "#E3E5E9","font-family": "'Roboto Condensed', serif", "line-height": "1.6", "font-size": "1.5em", "color": bgcolor_positive_or_negative(deltaa)},
 
@@ -557,10 +557,10 @@ with tab1:
             picture_position="left",
             image_aspect_ratio="16/9",
             image_object_fit="contain",
-            font_url="https://fonts.googleapis.com/css2?family=Old+Standard+TT:wght@400;700&family=Roboto+Condensed:wght@400&display=swap",
+            font_url="https://fonts.googleapis.com/css2?family=Roboto+Regular 400:wght@400;700&family=Roboto+Condensed:wght@400&display=swap",
             styles={
 
-                "title": {"font-family": "'Roboto Condensed', serif", "text-transform": "uppercase",   "font-weight": "700", "font-size": "0.8em", "color": "#E31E24" },
+                "title": {"font-family": "'Roboto Regular 400', serif", "text-transform": "uppercase",   "font-weight": "700", "font-size": "0.8em", "color": "#E31E24" },
                 "text": {"font-family": "'Roboto Condensed', serif", "line-height": "1.6", "font-size": "1.1em", "color": "#5d4037"},
                 "price": { "background-color": "#E3E5E9","font-family": "'Roboto Condensed', serif", "line-height": "1.6", "font-size": "1.5em", "color": bgcolor_positive_or_negative(deltaa)},
 
@@ -602,10 +602,10 @@ with tab1:
             picture_position="left",
             image_aspect_ratio="16/9",
             image_object_fit="contain",
-            font_url="https://fonts.googleapis.com/css2?family=Old+Standard+TT:wght@400;700&family=Roboto+Condensed:wght@400&display=swap",
+            font_url="https://fonts.googleapis.com/css2?family=Roboto+Regular 400:wght@400;700&family=Roboto+Condensed:wght@400&display=swap",
             styles={
 
-                "title": {"font-family": "'Roboto Condensed', serif", "text-transform": "uppercase",   "font-weight": "700", "font-size": "0.8em", "color": "#E31E24" },
+                "title": {"font-family": "'Roboto Regular 400', serif", "text-transform": "uppercase",   "font-weight": "700", "font-size": "0.8em", "color": "#E31E24" },
                 "text": {"font-family": "'Roboto Condensed', serif", "line-height": "1.6", "font-size": "1.1em", "color": "#5d4037"},
                 "price": { "background-color": "#E3E5E9","font-family": "'Roboto Condensed', serif", "line-height": "1.6", "font-size": "1.5em", "color": bgcolor_positive_or_negative(deltaa)},
 
