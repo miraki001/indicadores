7b1f5144582dee7cd531acf3255227128e4ed745
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-07-03 12:37:38 -0300

    Update 🍷Mercado Interno.py

--- pages/🍷Mercado Interno.py
+++ pages/🍷Mercado Interno.py
@@ -20,9 +20,6 @@ from despachos import desp_consumo
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-import pdfkit, os
-from fpdf import FPDF
-import base64
 import streamlit.components.v1 as components
 
 st.set_page_config(initial_sidebar_state="collapsed",
@@ -488,38 +485,6 @@ with tab1:
 
   st_echarts(options=option,key="otro" + str(dt.now()), height="400px")
 
-  export_as_pdf = st.button("Export Report")
-
-  def create_download_link(val, filename):
-    b64 = base64.b64encode(val)  # val looks like b'...'
-    return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="{filename}.pdf">Download file</a>'
-
-
-  if export_as_pdf:
-     pdf = FPDF()
-     pdf.add_page()
-     #pdf.image(tmpfile.name,10,10,100)
-     
-     html = create_download_link(pdf.output(dest="S").encode("latin-1"), "test")
-
-     st.markdown(html, unsafe_allow_html=True)
-  
-
-  st.markdown(
-    """
-    <style type="text/css" media="print">
-    div.page-break
-    {
-        page-break-after: always;
-        page-break-inside: avoid;
-    }
-    </style>
-    <div class="page-break">
-        <!-- Content goes here -->
-    </div>
-  """,
-    unsafe_allow_html=True,
-  ) 
   show_print_button ="""
     <script>
         function print_page(obj) {
