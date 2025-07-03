4e3b1a9d0eab60ea37041a11b681974b355de384
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-07-03 14:50:27 -0300

    Update exporta_pais.py

--- exportaciones/exporta_pais.py
+++ exportaciones/exporta_pais.py
@@ -760,13 +760,15 @@ def exporta_destino():
     input_color = 'blue'
     color_theme_list = ['blues', 'cividis', 'greens', 'inferno', 'magma', 'plasma', 'reds', 'rainbow', 'turbo', 'viridis']
     selected_color_theme = st.selectbox('Select a color theme', color_theme_list)
+    sc = 'greens'
     st.write(selected_color_theme)
     heatmap = alt.Chart(melted_df).mark_rect().encode(
             y=alt.Y(f'{'anio'}:O', axis=alt.Axis(title="Year", titleFontSize=18, titlePadding=15, titleFontWeight=900, labelAngle=0)),
             x=alt.X(f'{'variedad1'}:O', axis=alt.Axis(title="", titleFontSize=18, titlePadding=15, titleFontWeight=900)),
             color=alt.Color(f'max({'litros'}):Q',
                              legend=None,
-                             scale=alt.Scale(scheme=selected_color_theme)),
+                             #scale=alt.Scale(scheme=selected_color_theme)),
+                             scale=alt.Scale(scheme=sc)),
             stroke=alt.value('black'),
             strokeWidth=alt.value(0.25),
         ).properties(width=900
