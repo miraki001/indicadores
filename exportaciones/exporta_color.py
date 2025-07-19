d403e7bf1b16e3b458491573a13781fccbdbfac9
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-07-19 11:20:51 -0300

    Update exporta_color.py

--- exportaciones/exporta_color.py
+++ exportaciones/exporta_color.py
@@ -545,7 +545,7 @@ def exporta_color():
 		      )
     st.plotly_chart(fig, theme="streamlit")
     df["litros"] = df["litros"].astype(int)	
-    fig1 = px.sunburst(df, path=['color', 'tipo_envase', 'grupoenvase'], values='pop',
+    fig1 = px.sunburst(df, path=['color', 'tipo_envase', 'grupoenvase'], values='litros',
                   color='lifeExp', hover_data=['iso_alpha'],
                   color_continuous_scale='RdBu',
                   color_continuous_midpoint=np.average(df['litros'], weights=df['grupoenvase']))
