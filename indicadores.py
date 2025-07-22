6cf517d9c45164583d576ca3e8843ecb2a90702b
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-07-22 14:14:30 -0300

    Update indicadores.py

--- indicadores.py
+++ indicadores.py
@@ -74,7 +74,7 @@ hide_streamlit_style = """
             </style>
             """
 
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+#st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 #st.markdown(""" <style> #MainMenu {visibility: hidden;} footer {visibility: hidden;} </style> """, unsafe_allow_html=True)
 
