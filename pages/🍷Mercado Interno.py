8856a16905154ef2c1a1e872714dd3987e6fd15d
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-07-22 12:28:49 -0300

    Update 🍷Mercado Interno.py

--- pages/🍷Mercado Interno.py
+++ pages/🍷Mercado Interno.py
@@ -218,7 +218,7 @@ with tab1:
             st.caption("Selecciona uno o más Colores de la lista")
             color = st.multiselect("colord",  color_list, default=["Todos"],label_visibility="collapsed")      
     with col8:          
-        st.button(" ", icon=":material/print:", use_container_width=True,on_click=imprimir())
+        st.button(" ", icon=":material/print:", on_click=imprimir(),use_container_width=True)
 
         #show_print_button ="""
         #  <script>
