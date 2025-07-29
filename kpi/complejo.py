7f0dc4e97d19db223548187b49e4e14fd728da34
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-07-29 14:47:29 -0300

    Update complejo.py

--- kpi/complejo.py
+++ kpi/complejo.py
@@ -113,7 +113,7 @@ def complejo(dvex,dvdes,dvsup,dvcos,dvmosto):
                 "stack": "total",
                 "label": {"show": True},
                 "emphasis": {"focus": "series"},
-                "data": dres["litros"].tolist(),
+                "data": dex[].tolist(),
             },
             {
                 "name": "Despachos",
